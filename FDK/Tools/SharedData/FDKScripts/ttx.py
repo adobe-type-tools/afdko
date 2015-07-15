f3ec234b86b8cf4c2c371767be49188f8937d094
ReadRoberts <rroberts@adobe.com>
2015-07-15 10:46:43 -0700

    Replace the copy of ttx.py from the fontTools module with a wrapper file that calls the main function of the ttx.py module in the fontTools library. A minor maintenance fix.

--- FDK/Tools/SharedData/FDKScripts/ttx.py
+++ FDK/Tools/SharedData/FDKScripts/ttx.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python
 
+__copyright__ = """Copyright 2015 Adobe Systems Incorporated (http://www.adobe.com/). All Rights Reserved.
+"""
+
 # wrapper for the real ttx.py, which is in:
 # FDK/Tools/osx/Python/AFDKOPython27/lib/python2.7/site-packages/FontTools/fontTools/ttx.py
 # or the equivalent for win or linux os.
