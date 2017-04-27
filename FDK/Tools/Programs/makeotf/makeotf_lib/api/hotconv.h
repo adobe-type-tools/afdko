8727b9817a266b09e6cefb24ea5d5e504daffff8
ReadRoberts <rroberts@adobe.com>
2017-04-27 14:57:36 -0700

    makeotfexe. Fixed bug where makeotf would reject a name ID 25 record when specified in a feature file. This name id value used to be reserved, but is now used for for overriding the postscript family named used with arbitrary instances in variable fonts.

--- FDK/Tools/Programs/makeotf/makeotf_lib/api/hotconv.h
+++ FDK/Tools/Programs/makeotf/makeotf_lib/api/hotconv.h
@@ -10,7 +10,7 @@ This software is licensed as OpenSource, under the Apache License, Version 2.0. 
 extern "C" {
 #endif
 
-#define HOT_VERSION 0x0100635 /* Library version (1.0.95) */
+#define HOT_VERSION 0x0100636 /* Library version (1.0.96) */
 /*Warning: this string is now part of heuristic used by CoolType to identify the
 first round of CoolType fonts which had the backtrack sequence of a chaining 
 contextual substitution ordered incorrectly.  Fonts with the old ordering MUST match
