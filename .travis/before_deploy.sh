d5c1a3d6ce3ce715c86237ac2c864208db3cdbcd
Cosimo Lupo <cosimo@anthrotype.com>
2018-04-19 13:14:17 +0100

    deploy.sh: only check if .whl has already been built in dist
    for Linux, we only build the wheel; the sdist is only created in the OSX build.
    There is no need to create it twice, and there can only be one sdist on pypi anyway.

--- .travis/before_deploy.sh
+++ .travis/before_deploy.sh
@@ -7,8 +7,7 @@ set -x
 # Travis runs the `before_deploy` stage before each deployment, but
 # we only want to build them once, as we want to use the same
 # files for both Github and PyPI
-if $(ls ./dist/afdko*.zip > /dev/null 2>&1) && \
-		$(ls ./dist/afdko*.whl > /dev/null 2>&1); then
+if $(ls ./dist/afdko*.whl > /dev/null 2>&1); then
 	echo "Distribution packages already exists; skipping"
 else
 	if [ "$TRAVIS_OS_NAME" == "linux" ]; then
