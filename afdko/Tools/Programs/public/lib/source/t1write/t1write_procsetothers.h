55cc25d3ef97bb5c670f67110d877a0ecf2f2128
Miguel Sousa <msousa@adobe.com>
2018-04-23 18:26:00 -0700

    [t1write] Remove superfluous .txt files. Add copyright and license notice to .h files
    Fixes #332, #172

--- afdko/Tools/Programs/public/lib/source/t1write/t1write_procsetothers.h
+++ afdko/Tools/Programs/public/lib/source/t1write/t1write_procsetothers.h
@@ -1,2 +1,5 @@
+/* Copyright 2014 Adobe Systems Incorporated (http://www.adobe.com/). All Rights Reserved.
+This software is licensed as OpenSource, under the Apache License, Version 2.0. This license is available at: http://opensource.org/licenses/Apache-2.0. */
+
 "/OtherSubrs [{ct_CffDict/F0Subr get exec} {ct_CffDict/F1Subr get exec}\n"
 "{ct_CffDict/F2Subr get exec} {ct_CffDict/HSSubr get exec}] def\n"
