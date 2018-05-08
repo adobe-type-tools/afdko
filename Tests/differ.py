f82645cd7ab28a109f1795dadd85f9c37ac5aab5
Miguel Sousa <msousa@adobe.com>
2018-05-08 12:12:34 -0700

    Integration testing framework
    Fixes #338

---
+++ Tests/differ.py
@@ -0,0 +1,339 @@
+#!/usr/bin/env python
+
+# Copyright 2018 Adobe. All rights reserved.
+
+"""
+Helper script for diff'ing files.
+Used as part of the integration tests.
+"""
+
+from __future__ import print_function, division, absolute_import
+
+import argparse
+import difflib
+import filecmp
+import logging
+import os
+import sys
+
+from fontTools.misc.py23 import open
+
+__version__ = '0.1.1'
+
+logger = logging.getLogger(__file__)
+
+
+TXT_MODE = 'txt'
+BIN_MODE = 'bin'
+SPLIT_MARKER = '_+:+_'
+
+
+def _line_to_skip(skip_strings, line):
+    """
+    Loops over the skip items.
+    Returns True if the beginning of the line matches a skip item.
+    """
+    for item in skip_strings:
+        if line.startswith(item):
+            return True
+    return False
+
+
+def _read_txt_file(path, skip_strings=(), skip_lines=()):
+    """
+    Reads a text file and returns a list of its lines.
+    'skip_strings' is a list of strings containing the starts of lines to skip.
+    'skip_lines' is a list of integers for the line numbers to skip.
+    """
+    # Hard code a first line; this way the difflib results start from 1 instead
+    # of zero, thus matching the file's line numbers
+    lines = ['']
+    with open(path, "r", encoding="utf-8") as f:
+        for i, line in enumerate(f.readlines(), 1):
+            # Skip lines that change, such as timestamps
+            if _line_to_skip(skip_strings, line):
+                logger.debug(
+                    "Matched begin of line. Skipped: {}".format(line.rstrip()))
+                # Blank the line instead of actually skipping (via 'continue');
+                # this way the difflib results show the correct line numbers
+                line = ''
+            # Skip specific lines, referenced by number
+            elif i in skip_lines:
+                logger.debug(
+                    "Matched line #{}. Skipped: {}".format(i, line.rstrip()))
+                # Blank the line instead of actually skipping (via 'continue');
+                # this way the difflib results show the correct line numbers
+                line = ''
+            # Use os-native line separator to enable running difflib
+            lines.append(line.rstrip() + os.linesep)
+    return lines
+
+
+def _diff_txt_files(path1, path2, skip_strings=(), skip_lines=()):
+    """
+    Diffs two text-based files using difflib.
+    Returns True if the contents match, and False if they don't.
+    """
+    expected = _read_txt_file(path1, skip_strings, skip_lines)
+    actual = _read_txt_file(path2, skip_strings, skip_lines)
+    if actual != expected:
+        for line in difflib.unified_diff(
+                expected, actual, fromfile=path1, tofile=path2, n=1):
+            sys.stdout.write(line)
+        return False
+    return True
+
+
+def _get_all_file_paths_in_dir_tree(start_path):
+    """
+    Returns a list of relative paths of all files in a directory tree.
+    The list's items are ordered top-down according to the tree.
+    """
+    all_paths = []
+    for dir_name, _, file_names in os.walk(start_path):
+        all_paths.extend(
+            [os.path.join(dir_name, f_name) for f_name in file_names])
+
+    # Make the paths relative
+    all_paths = [path.replace(start_path, '') for path in all_paths]
+
+    logger.debug("All paths: {}".format(all_paths))
+
+    return all_paths
+
+
+def _compare_dir_contents(path1, path2):
+    """
+    Checks if two directory trees have the same files and folders.
+    Returns a list of relative paths to all files if the dirs' contents match,
+    and None if they don't.
+    """
+    all_paths1 = _get_all_file_paths_in_dir_tree(path1)
+    all_paths2 = _get_all_file_paths_in_dir_tree(path2)
+    if all_paths1 != all_paths2:
+        logger.debug("Folders' contents don't match.")
+        return None
+    return all_paths1
+
+
+def _diff_dirs(dirpath1, dirpath2, skip_strings=(), skip_lines=(),
+               mode=TXT_MODE):
+    """
+    Diffs two folders containing files.
+    Returns True if all files match. Returns False if the folders' contents
+    don't match, or as soon as one non-matching file is found.
+    """
+    all_rel_file_paths = _compare_dir_contents(dirpath1, dirpath2)
+    if all_rel_file_paths is None:
+        return False
+    for rel_file_path in all_rel_file_paths:
+        path1 = dirpath1 + rel_file_path
+        assert os.path.exists(path1), "Not a valid path1: {}".format(path1)
+        path2 = dirpath2 + rel_file_path
+        assert os.path.exists(path2), "Not a valid path2: {}".format(path2)
+        if mode == BIN_MODE:
+            diff_result = filecmp.cmp(path1, path2)
+        else:
+            diff_result = _diff_txt_files(path1, path2,
+                                          skip_strings, skip_lines)
+        if not diff_result:
+            logger.debug("Non-matching file: {}".format(rel_file_path))
+            return False
+    return True
+
+
+def diff_paths(opts):
+    """
+    Diffs the contents of two paths using the parameters provided.
+    Returns True if the contents match, and False if they don't.
+    """
+    if opts.mode == TXT_MODE:
+        if os.path.isfile(opts.path1):
+            return _diff_txt_files(opts.path1, opts.path2,
+                                   opts.skip_strings, opts.skip_lines)
+        elif os.path.isdir(opts.path1):
+            return _diff_dirs(opts.path1, opts.path2,
+                              opts.skip_strings, opts.skip_lines)
+    elif opts.mode == BIN_MODE:
+        if os.path.isfile(opts.path1):
+            return filecmp.cmp(opts.path1, opts.path2)
+        elif os.path.isdir(opts.path1):
+            return _diff_dirs(opts.path1, opts.path2, mode=opts.mode)
+
+
+def _get_path_kind(pth):
+    """
+    Returns a string describing the kind of path.
+    String values are file, folder, invalid, and unknown.
+    """
+    if not os.path.exists(pth):
+        return 'invalid'
+    elif os.path.isfile(pth):
+        return 'file'
+    elif os.path.isdir(pth):
+        return 'folder'
+    return 'unknown'
+
+
+def _paths_are_same_kind(path1, path2):
+    """
+    Checks that both paths are either files or folders.
+    Returns boolean.
+    """
+    if all([os.path.isfile(path) for path in (path1, path2)]):
+        return True
+    elif all([os.path.isdir(path) for path in (path1, path2)]):
+        return True
+    return False
+
+
+def _validate_path(path_str):
+    valid_path = os.path.abspath(os.path.realpath(path_str))
+    if not os.path.exists(valid_path):
+        raise argparse.ArgumentTypeError(
+            "{} is not a valid path.".format(path_str))
+    return valid_path
+
+
+def _split_string_sequence(str_seq):
+    return tuple(str_seq.split(SPLIT_MARKER))
+
+
+def _split_num_range_or_delta(num_str):
+    num_range = num_str.split('-') + ['-']
+    num_delta = num_str.split('+') + ['+']
+    if len(num_range) == 3:
+        return num_range
+    elif len(num_delta) == 3:
+        return num_delta
+    return num_str
+
+
+def _convert_to_int(num_str):
+    try:
+        return int(num_str)
+    except ValueError:
+        raise argparse.ArgumentTypeError("Not a number: {}".format(num_str))
+
+
+def _expand_num_range_or_delta(num_str_lst):
+    start_num = _convert_to_int(num_str_lst[0])
+    rng_dlt_num = _convert_to_int(num_str_lst[1])
+    plus_minus = num_str_lst[2]
+    if plus_minus == '+':
+        return list(range(start_num, start_num + rng_dlt_num + 1))
+    elif plus_minus == '-':
+        if not (rng_dlt_num >= start_num):
+            raise argparse.ArgumentTypeError(
+                "The start of range value is larger than the end of range "
+                "value: {}-{}".format(start_num, rng_dlt_num))
+        return list(range(start_num, rng_dlt_num + 1))
+    else:
+        raise argparse.ArgumentTypeError(
+            "Unrecognized number separator: {}".format(plus_minus))
+
+
+def _convert_seq_to_ints(num_seq):
+    seq = []
+    for item in num_seq:
+        if isinstance(item, list):
+            seq.extend(_expand_num_range_or_delta(item))
+        else:
+            seq.append(_convert_to_int(item))
+    return seq
+
+
+def _split_linenumber_sequence(str_seq):
+    num_seq = [_split_num_range_or_delta(item) for item in str_seq.split(',')]
+    return tuple(_convert_seq_to_ints(num_seq))
+
+
+def get_options(args):
+    parser = argparse.ArgumentParser(
+        formatter_class=argparse.RawTextHelpFormatter,
+        description=__doc__
+    )
+    parser.add_argument(
+        '--version',
+        action='version',
+        version=__version__
+    )
+    parser.add_argument(
+        '-v',
+        '--verbose',
+        action='count',
+        default=0,
+        help='verbose mode\n'
+             'Use -vv for debug mode'
+    )
+    parser.add_argument(
+        '-m',
+        '--mode',
+        default=TXT_MODE,
+        choices=(TXT_MODE, BIN_MODE),
+        help='diff mode (default: %(default)s)'
+    )
+    parser.add_argument(
+        '-s',
+        '--string',
+        dest='skip_strings',
+        type=_split_string_sequence,
+        default=(),
+        help='string for matching the beginning of a line to skip\n'
+             'For multiple strings, separate them with {}'.format(SPLIT_MARKER)
+    )
+    parser.add_argument(
+        '-l',
+        '--line',
+        dest='skip_lines',
+        type=_split_linenumber_sequence,
+        default=(),
+        help='number of a line to skip\n'
+             'For multiple line numbers, separate them with a comma (,).\n'
+             'For ranges of lines, use a minus (-) between two numbers.\n'
+             'For a line delta, use a plus (+) between two numbers.'
+    )
+    parser.add_argument(
+        'path1',
+        metavar='PATH1',
+        type=_validate_path,
+        help='1st path for comparison'
+    )
+    parser.add_argument(
+        'path2',
+        metavar='PATH2',
+        type=_validate_path,
+        help='2nd path for comparison'
+    )
+    options = parser.parse_args(args)
+
+    if not options.verbose:
+        level = "WARNING"
+    elif options.verbose == 1:
+        level = "INFO"
+    else:
+        level = "DEBUG"
+    logging.basicConfig(level=level)
+
+    if not _paths_are_same_kind(options.path1, options.path2):
+        parser.error("The paths are not of the same kind. Path1's kind is {}. "
+                     "Path2's kind is {}.".format(
+                         _get_path_kind(options.path1),
+                         _get_path_kind(options.path2)))
+
+    logger.debug("Line numbers: {}".format(options.skip_lines))
+
+    return options
+
+
+def main(args=None):
+    """
+    Returns True if the inputs match, and False if they don't.
+    """
+    opts = get_options(args)
+
+    return diff_paths(opts)
+
+
+if __name__ == "__main__":
+    sys.exit(0 if main() else 1)
---
+++ Tests/runner.py
@@ -0,0 +1,210 @@
+#!/usr/bin/env python
+
+# Copyright 2018 Adobe. All rights reserved.
+
+"""
+Helper script for running a command line tool.
+Used as part of the integration tests.
+"""
+
+from __future__ import print_function, division, absolute_import
+
+import argparse
+import logging
+import os
+import platform
+import subprocess
+import sys
+import tempfile
+
+__version__ = '0.1.1'
+
+logger = logging.getLogger(__file__)
+
+
+def _write_file(file_path, data):
+    with open(file_path, "wb") as f:
+        f.write(data)
+
+
+def _get_input_dir_path(tool_name):
+    tool_name = tool_name.split('.')[0]  # Windows tool name contains '.exe'
+    input_dir = os.path.join(os.path.split(__file__)[0], tool_name + '_data',
+                             'input')
+    return os.path.abspath(os.path.realpath(input_dir))
+
+
+def _get_save_location(save_path):
+    """
+    If save_path is not provided ('--save' option wasn't used),
+    create a temporary location.
+    """
+    if not save_path:
+        save_path = tempfile.NamedTemporaryFile(delete=False).name
+    return save_path
+
+
+def run_tool(opts):
+    """
+    Runs the tool using the parameters provided.
+    """
+    input_dir = _get_input_dir_path(opts.tool)
+    save_loc = _get_save_location(opts.save_path)
+
+    args = []
+    for opt in opts.options:
+        if opt.startswith('='):
+            args.append('--{}'.format(opt[1:]))
+        elif opt.startswith('*'):
+            args.append('+{}'.format(opt[1:]))
+        elif opt.startswith('_'):
+            args.append('{}'.format(opt[1:]))
+        else:
+            args.append('-{}'.format(opt))
+
+    args.insert(0, opts.tool)
+
+    if opts.files:
+        files = [os.path.join(input_dir, fname) for fname in opts.files]
+        assert all([os.path.exists(fpath) for fpath in files]), (
+            "Invalid input path found.")
+        args.extend(files)
+
+    args.append(save_loc)
+
+    logger.debug(
+        "About to run the command below\n==>{}<==".format(' '.join(args)))
+    try:
+        output = subprocess.check_output(args)
+        if opts.redirect:
+            _write_file(save_loc, output)
+        return save_loc
+    except (subprocess.CalledProcessError, OSError) as err:
+        logger.error(err)
+        return None
+
+
+def _check_tool(tool_name):
+    """
+    Checks if the invoked tool produces a known command.
+    Returns the tool's name if the check is successful,
+    or a tuple with the tool's name and the error if it's not.
+    """
+    if platform.system() == 'Windows':
+        tool_name += '.exe'
+    try:
+        subprocess.check_output([tool_name, '-h'])
+        return tool_name
+    except (subprocess.CalledProcessError, OSError) as err:
+        logger.error(err)
+        return tool_name, err
+
+
+def _check_save_path(path_str):
+    test_path = os.path.abspath(os.path.realpath(path_str))
+    del_test_file = True
+    try:
+        if os.path.exists(test_path):
+            del_test_file = False
+        open(test_path, 'a').close()
+        if del_test_file:
+            os.remove(test_path)
+    except (IOError, OSError):
+        raise argparse.ArgumentTypeError(
+            "{} is not a valid path to write to.".format(test_path))
+    return test_path
+
+
+def get_options(args):
+    parser = argparse.ArgumentParser(
+        formatter_class=argparse.RawTextHelpFormatter,
+        description=__doc__
+    )
+    parser.add_argument(
+        '--version',
+        action='version',
+        version=__version__
+    )
+    parser.add_argument(
+        '-v',
+        '--verbose',
+        action='count',
+        default=0,
+        help='verbose mode\n'
+             'Use -vv for debug mode'
+    )
+    parser.add_argument(
+        '-t',
+        '--tool',
+        required=True,
+        type=_check_tool,
+        metavar='NAME',
+        help='name of the tool to run'
+    )
+    parser.add_argument(
+        '-o',
+        '--options',
+        nargs='+',
+        metavar='OPTION',
+        default=[],
+        help='options to run the tool with\n'
+             'By default the option will be prepended with one minus (-).\n'
+             'To request the use of two minus (--), prepend the option with '
+             'an equal (=).\n'
+             'To request the use of a plus (+), prepend the option with an '
+             'asterisk (*).\n'
+             'To request the use of nothing, prepend the option with an '
+             'underscore (_).'
+    )
+    parser.add_argument(
+        '-f',
+        '--files',
+        nargs='+',
+        metavar='FILE_NAME',
+        help='names of the files to provide to the tool\n'
+             "The files will be sourced from the 'input' folder inside the "
+             "'{tool_name}_data' directory."
+    )
+    parser.add_argument(
+        '-s',
+        '--save',
+        dest='save_path',
+        type=_check_save_path,
+        metavar='PATH',
+        help="path to save the tool's output to\n"
+             'The default is to use a temporary location.'
+    )
+    parser.add_argument(
+        '-r',
+        '--redirect',
+        action='store_true',
+        help="redirect the tool's output to a file"
+    )
+    options = parser.parse_args(args)
+
+    if not options.verbose:
+        level = "WARNING"
+    elif options.verbose == 1:
+        level = "INFO"
+    else:
+        level = "DEBUG"
+    logging.basicConfig(level=level)
+
+    if isinstance(options.tool, tuple):
+        parser.error("'{}' is an unknown command.".format(options.tool[0]))
+
+    return options
+
+
+def main(args=None):
+    """
+    Returns the path to the result/output file if the command is successful,
+    and None if it isn't.
+    """
+    opts = get_options(args)
+
+    return run_tool(opts)
+
+
+if __name__ == "__main__":
+    sys.exit(0 if main() is not None else 1)
