7ff6cc6a1d0892c082b29fecb3122dc0935209f5
Miguel Sousa <msousa@adobe.com>
2018-05-13 11:10:17 -0700

    More ttxn integration tests

--- Tests/ttxn_test.py
+++ Tests/ttxn_test.py
@@ -10,6 +10,9 @@ TOOL = 'ttxn'
 CMD = ['-t', TOOL]
 
 OTF_FONT = 'OTF.otf'
+OTF2_FONT = 'OTF2.otf'
+OTF3_FONT = 'OTF3.otf'
+TTF_FONT = 'TTF.ttf'
 
 
 def _get_expected_path(file_name):
@@ -53,5 +56,50 @@ def test_dump_otf_gsub_only():
         runner(CMD + ['-o', 'tGSUB', 'o{}'.format(tmp.name),
                       '-f', OTF_FONT, '-n'])
         expected_path = _get_expected_path('OTF_GSUB_only.ttx')
+        assert differ([expected_path, tmp.name,
+                       '-s', '<ttFont sfntVersion']) is True
+
+
+def test_dump_otf2_gpos_only():
+    with tempfile.NamedTemporaryFile(delete=False) as tmp:
+        runner(CMD + ['-o', 'tGPOS', 'o{}'.format(tmp.name),
+                      '-f', OTF2_FONT, '-n'])
+        expected_path = _get_expected_path('OTF2_GPOS_only.ttx')
+        assert differ([expected_path, tmp.name,
+                       '-s', '<ttFont sfntVersion']) is True
+
+
+def test_dump_otf2_gsub_only():
+    with tempfile.NamedTemporaryFile(delete=False) as tmp:
+        runner(CMD + ['-o', 'tGSUB', 'o{}'.format(tmp.name),
+                      '-f', OTF2_FONT, '-n'])
+        expected_path = _get_expected_path('OTF2_GSUB_only.ttx')
+        assert differ([expected_path, tmp.name,
+                       '-s', '<ttFont sfntVersion']) is True
+
+
+def test_dump_otf3_gpos_only():
+    with tempfile.NamedTemporaryFile(delete=False) as tmp:
+        runner(CMD + ['-o', 'tGPOS', 'o{}'.format(tmp.name),
+                      '-f', OTF3_FONT, '-n'])
+        expected_path = _get_expected_path('OTF3_GPOS_only.ttx')
+        assert differ([expected_path, tmp.name,
+                       '-s', '<ttFont sfntVersion']) is True
+
+
+def test_dump_otf3_gsub_only():
+    with tempfile.NamedTemporaryFile(delete=False) as tmp:
+        runner(CMD + ['-o', 'tGSUB', 'o{}'.format(tmp.name),
+                      '-f', OTF3_FONT, '-n'])
+        expected_path = _get_expected_path('OTF3_GSUB_only.ttx')
+        assert differ([expected_path, tmp.name,
+                       '-s', '<ttFont sfntVersion']) is True
+
+
+def test_dump_ttf():
+    with tempfile.NamedTemporaryFile(delete=False) as tmp:
+        runner(CMD + ['-o', 'o{}'.format(tmp.name),
+                      '-f', TTF_FONT, '-n'])
+        expected_path = _get_expected_path('TTF.ttx')
         assert differ([expected_path, tmp.name,
                        '-s', '<ttFont sfntVersion']) is True
