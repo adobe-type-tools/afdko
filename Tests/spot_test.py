ff7d7452f78c45ced8c2c18ad1c240b9c4491950
Miguel Sousa <msousa@adobe.com>
2018-05-13 18:40:23 -0700

    More spot integration tests

--- Tests/spot_test.py
+++ Tests/spot_test.py
@@ -4,6 +4,7 @@ import os
 
 from runner import main as runner
 from differ import main as differ
+from differ import SPLIT_MARKER
 
 TOOL = 'spot'
 CMD = ['-t', TOOL]
@@ -32,11 +33,286 @@ def test_list_features():
     assert differ([expected_path, actual_path]) is True
 
 
-def test_glyph_proof():
+def test_dump_base_equal_4():
+    actual_path = runner(CMD + ['-r', '-o', 't', '_BASE=4', '-f', BLACK])
+    expected_path = _get_expected_path('dump_BASE=4.txt')
+    assert differ([expected_path, actual_path]) is True
+
+
+def test_dump_base_equal_5():
+    actual_path = runner(CMD + ['-r', '-o', 't', '_BASE=5', '-f', BLACK])
+    expected_path = _get_expected_path('dump_BASE=5.txt')
+    assert differ([expected_path, actual_path]) is True
+
+
+def test_dump_gdef_equal_7():
+    actual_path = runner(CMD + ['-r', '-o', 't', '_GDEF=7', '-f', BLACK])
+    expected_path = _get_expected_path('dump_GDEF=7.txt')
+    assert differ([expected_path, actual_path]) is True
+
+
+def test_dump_gpos_equal_5():
+    actual_path = runner(CMD + ['-r', '-o', 't', '_GPOS=5', '-f', BLACK])
+    expected_path = _get_expected_path('dump_GPOS=5.txt')
+    assert differ([expected_path, actual_path]) is True
+
+
+# Segmentation fault: 11
+# https://github.com/adobe-type-tools/afdko/issues/371
+# def test_dump_gpos_equal_6():
+#     actual_path = runner(CMD + ['-r', '-o', 't', '_GPOS=6', '-f', BLACK])
+#     expected_path = _get_expected_path('dump_GPOS=6.txt')
+#     assert differ([expected_path, actual_path]) is True
+
+
+def test_dump_gpos_equal_7():
+    actual_path = runner(CMD + ['-r', '-o', 't', '_GPOS=7', '-f', BLACK])
+    expected_path = _get_expected_path('dump_GPOS=7.txt')
+    assert differ([expected_path, actual_path]) is True
+
+
+def test_dump_gpos_equal_8():
+    actual_path = runner(CMD + ['-r', '-o', 't', '_GPOS=8', '-f', BLACK])
+    expected_path = _get_expected_path('dump_GPOS=8.ps')
+    assert differ([expected_path, actual_path]) is True
+
+
+def test_dump_gsub_equal_4():
+    actual_path = runner(CMD + ['-r', '-o', 't', '_GSUB=4', '-f', BLACK])
+    expected_path = _get_expected_path('dump_GSUB=4.txt')
+    assert differ([expected_path, actual_path]) is True
+
+
+def test_dump_gsub_equal_5():
+    actual_path = runner(CMD + ['-r', '-o', 't', '_GSUB=5', '-f', BLACK])
+    expected_path = _get_expected_path('dump_GSUB=5.txt')
+    assert differ([expected_path, actual_path]) is True
+
+
+def test_dump_gsub_equal_7():
+    actual_path = runner(CMD + ['-r', '-o', 't', '_GSUB=7', '-f', BLACK])
+    expected_path = _get_expected_path('dump_GSUB=7.txt')
+    assert differ([expected_path, actual_path]) is True
+
+
+def test_dump_gsub_equal_8():
+    actual_path = runner(CMD + ['-r', '-o', 't', '_GSUB=8', '-f', BLACK])
+    expected_path = _get_expected_path('dump_GSUB=8.ps')
+    assert differ([expected_path, actual_path]) is True
+
+
+def test_bug373_dump_gsub_equal_7_long_glyph_name_otf():
+    actual_path = runner(CMD + ['-r', '-o', 't', '_GSUB=7',
+                                '-f', 'long_glyph_name.otf'])
+    expected_path = _get_expected_path('bug373_otf.txt')
+    assert differ([expected_path, actual_path]) is True
+
+
+# Abort trap: 6
+# https://github.com/adobe-type-tools/afdko/issues/373
+# def test_bug373_dump_gsub_equal_7_long_glyph_name_ttf():
+#     actual_path = runner(CMD + ['-r', '-o', 't', '_GSUB=7',
+#                                 '-f', 'long_glyph_name.ttf'])
+#     expected_path = _get_expected_path('bug373_ttf.txt')
+#     assert differ([expected_path, actual_path]) is True
+
+
+def test_dump_cmap_equal_5():
+    actual_path = runner(CMD + ['-r', '-o', 't', '_cmap=5', '-f', BLACK])
+    expected_path = _get_expected_path('dump_cmap=5.txt')
+    assert differ([expected_path, actual_path]) is True
+
+
+def test_dump_cmap_equal_6():
+    actual_path = runner(CMD + ['-r', '-o', 't', '_cmap=6', '-f', BLACK])
+    expected_path = _get_expected_path('dump_cmap=6.txt')
+    assert differ([expected_path, actual_path]) is True
+
+
+def test_dump_cmap_equal_7():
+    actual_path = runner(CMD + ['-r', '-o', 't', '_cmap=7', '-f', BLACK])
+    expected_path = _get_expected_path('dump_cmap=7.txt')
+    assert differ([expected_path, actual_path]) is True
+
+
+def test_dump_cmap_equal_8():
+    actual_path = runner(CMD + ['-r', '-o', 't', '_cmap=8', '-f', BLACK])
+    expected_path = _get_expected_path('dump_cmap=8.txt')
+    assert differ([expected_path, actual_path]) is True
+
+
+def test_dump_cmap_equal_9():
+    actual_path = runner(CMD + ['-r', '-o', 't', '_cmap=9', '-f', BLACK])
+    expected_path = _get_expected_path('dump_cmap=9.ps')
+    assert differ([expected_path, actual_path]) is True
+
+
+def test_dump_cmap_equal_10():
+    actual_path = runner(CMD + ['-r', '-o', 't', '_cmap=10', '-f', BLACK])
+    expected_path = _get_expected_path('dump_cmap=10.ps')
+    assert differ([expected_path, actual_path]) is True
+
+
+def test_dump_cmap_equal_11():
+    actual_path = runner(CMD + ['-r', '-o', 't', '_cmap=11', '-f', BLACK])
+    expected_path = _get_expected_path('dump_cmap=11.txt')
+    assert differ([expected_path, actual_path]) is True
+
+
+def test_dump_cff_equal_6():
+    actual_path = runner(CMD + ['-r', '-o', 't', '_CFF_=6', '-f', BLACK])
+    expected_path = _get_expected_path('dump_CFF_=6.ps')
+    assert differ([expected_path, actual_path,
+                   '-s',
+                   '318 764 moveto (' + SPLIT_MARKER +
+                   '72 750 moveto ('
+                   ]) is True
+
+
+def test_dump_cff_equal_6a():
+    actual_path = runner(CMD + ['-r', '-o', 't', '_CFF_=6', 'a', '-f', BLACK])
+    expected_path = _get_expected_path('dump_CFF_=6a.ps')
+    assert differ([expected_path, actual_path,
+                   '-s',
+                   '318 764 moveto (' + SPLIT_MARKER +
+                   '72 750 moveto ('
+                   ]) is True
+
+
+def test_dump_cff_equal_6c():
+    actual_path = runner(CMD + ['-r', '-o', 't', '_CFF_=6', 'c', '-f', BLACK])
+    expected_path = _get_expected_path('dump_CFF_=6c.ps')
+    assert differ([expected_path, actual_path,
+                   '-s',
+                   '318 764 moveto (' + SPLIT_MARKER +
+                   '72 750 moveto ('
+                   ]) is True
+
+
+def test_dump_cff_equal_6r():
+    actual_path = runner(CMD + ['-r', '-o', 't', '_CFF_=6', 'R', '-f', BLACK])
+    expected_path = _get_expected_path('dump_CFF_=6R.ps')
+    assert differ([expected_path, actual_path,
+                   '-s',
+                   '318 764 moveto (' + SPLIT_MARKER +
+                   '72 750 moveto ('
+                   ]) is True
+
+
+def test_dump_cff_equal_6g():
+    actual_path = runner(CMD + ['-r', '-o', 't', '_CFF_=6', 'g2,5-7',
+                                '-f', BLACK])
+    expected_path = _get_expected_path('dump_CFF_=6g.ps')
+    assert differ([expected_path, actual_path,
+                   '-s',
+                   '318 764 moveto (' + SPLIT_MARKER +
+                   '72 750 moveto ('
+                   ]) is True
+
+
+# Fails on Windows, passes on Mac
+# https://ci.appveyor.com/project/adobe-type-tools/afdko/build/1.0.217
+# https://travis-ci.org/adobe-type-tools/afdko/builds/378601819
+# def test_dump_cff_equal_6s():
+#     actual_path = runner(CMD + ['-r', '-o', 't', '_CFF_=6', 's1.5,0.5',
+#                                 '-f', BLACK])
+#     expected_path = _get_expected_path('dump_CFF_=6s.ps')
+#     assert differ([expected_path, actual_path,
+#                    '-s',
+#                    '318 764 moveto (' + SPLIT_MARKER +
+#                    '72 750 moveto ('
+#                    ]) is True
+
+
+def test_dump_cff_equal_6b():
+    actual_path = runner(CMD + ['-r', '-o', 't', '_CFF_=6', 'b-10,-10,200,500',
+                                'g4', '-f', BLACK])
+    expected_path = _get_expected_path('dump_CFF_=6b.ps')
+    assert differ([expected_path, actual_path,
+                   '-s',
+                   '318 764 moveto (' + SPLIT_MARKER +
+                   '72 750 moveto ('
+                   ]) is True
+
+
+def test_dump_cff_equal_7():  # same as test_glyph_proof
+    actual_path = runner(CMD + ['-r', '-o', 't', '_CFF_=7', '-f', BLACK])
+    expected_path = _get_expected_path('dump_CFF_=7.ps')
+    assert differ([expected_path, actual_path,
+                   '-s', '0 743.4 moveto (CFF:']) is True
+
+
+def test_dump_cff_equal_7g():
+    actual_path = runner(CMD + ['-r', '-o', 't', '_CFF_=7', 'g2,5-7',
+                                '-f', BLACK])
+    expected_path = _get_expected_path('dump_CFF_=7g.ps')
+    assert differ([expected_path, actual_path,
+                   '-s', '0 743.4 moveto (CFF:']) is True
+
+
+def test_dump_cff_equal_8():
+    actual_path = runner(CMD + ['-r', '-o', 't', '_CFF_=8', '-f', BLACK])
+    expected_path = _get_expected_path('dump_CFF_=8.ps')
+    assert differ([expected_path, actual_path]) is True
+
+
+def test_dump_cff_equal_8g():
+    actual_path = runner(CMD + ['-r', '-o', 't', '_CFF_=8', 'g2,5-7',
+                                '-f', BLACK])
+    expected_path = _get_expected_path('dump_CFF_=8g.ps')
+    assert differ([expected_path, actual_path]) is True
+
+
+def test_dump_hmtx_equal_5():
+    actual_path = runner(CMD + ['-r', '-o', 't', '_hmtx=5', '-f', BLACK])
+    expected_path = _get_expected_path('dump_hmtx=5.txt')
+    assert differ([expected_path, actual_path]) is True
+
+
+def test_dump_hmtx_equal_6():
+    actual_path = runner(CMD + ['-r', '-o', 't', '_hmtx=6', '-f', BLACK])
+    expected_path = _get_expected_path('dump_hmtx=6.txt')
+    assert differ([expected_path, actual_path]) is True
+
+
+def test_dump_hmtx_equal_7():
+    actual_path = runner(CMD + ['-r', '-o', 't', '_hmtx=7', '-f', BLACK])
+    expected_path = _get_expected_path('dump_hmtx=7.txt')
+    assert differ([expected_path, actual_path]) is True
+
+
+def test_dump_hmtx_equal_8():
+    actual_path = runner(CMD + ['-r', '-o', 't', '_hmtx=8', '-f', BLACK])
+    expected_path = _get_expected_path('dump_hmtx=8.txt')
+    assert differ([expected_path, actual_path]) is True
+
+
+def test_dump_name_equal_2():
+    actual_path = runner(CMD + ['-r', '-o', 't', '_name=2', '-f', BLACK])
+    expected_path = _get_expected_path('dump_name=2.txt')
+    assert differ([expected_path, actual_path]) is True
+
+
+# Fails on Windows, passes on Mac
+# https://ci.appveyor.com/project/adobe-type-tools/afdko/build/1.0.217
+# https://travis-ci.org/adobe-type-tools/afdko/builds/378601819
+# def test_dump_name_equal_3():
+#     actual_path = runner(CMD + ['-r', '-o', 't', '_name=3', '-f', BLACK])
+#     expected_path = _get_expected_path('dump_name=3.txt')
+#     assert differ([expected_path, actual_path]) is True
+
+
+def test_dump_name_equal_4():
+    actual_path = runner(CMD + ['-r', '-o', 't', '_name=4', '-f', BLACK])
+    expected_path = _get_expected_path('dump_name=4.txt')
+    assert differ([expected_path, actual_path]) is True
+
+
+def test_glyph_proof():  # same as test_dump_cff_equal_7
     actual_path = runner(CMD + ['-r', '-o', 'G', '-f', BLACK])
     expected_path = _get_expected_path('glyph_proof.ps')
     assert differ([expected_path, actual_path,
-                   '-s', '0 743.4 moveto (CFF:black.otf']) is True
+                   '-s', '0 743.4 moveto (CFF:']) is True
 
 
 def test_glyph_proof_no_header():
