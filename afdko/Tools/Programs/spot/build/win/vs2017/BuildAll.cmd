6580e0ebacf588e0ade05ffb3021f77e865eedfc
ReadRoberts <rroberts@adobe.com>
2018-05-23 15:51:22 -0700

    [spot] Update Windows build command file for VS 2017 to build with any of several types of VS 2017.
    The Appveyor batch process failed with the more complex command file syntax using vswhere.exe to locate the msbuild.exe file. Try a very simple approach.

--- afdko/Tools/Programs/spot/build/win/vs2017/BuildAll.cmd
+++ afdko/Tools/Programs/spot/build/win/vs2017/BuildAll.cmd
@@ -4,20 +4,28 @@ set targetProgram=spot
 
 if "%1"=="" set buildConfig="Build"
 
-REM Find location of latest VS installer. Works with only VS 2017 and later.
-set vswbin="%ProgramFiles(x86)%\Microsoft Visual Studio\Installer\vswhere.exe"
-for /f "usebackq tokens=1* delims=: " %%a in (`%vswbin% -latest -requires Microsoft.Component.MSBuild`) do (
-    if /i "%%a"=="installationPath" set vspath=%%b
+echo ON
+
+set msbuildPath="C:\Program Files (x86)\Microsoft Visual Studio\2017\BuildTools\MSBuild\15.0\Bin\msbuild.exe"
+
+if not exist %msbuildPath% (
+set msbuildPath="C:\Program Files (x86)\Microsoft Visual Studio\2017\Enterprise\MSBuild\15.0\Bin\msbuild.exe"
 )
 
-echo ON
-set msbuildPath=%vspath%\MSBuild\15.0\Bin
+if not exist %msbuildPath% (
+set msbuildPath="C:\Program Files (x86)\Microsoft Visual Studio\2017\Professional\MSBuild\15.0\Bin\msbuild.exe"
+)
 
-type %msbuildPath%\msbuild.exe
-dir %msbuildPath%\msbuild.exe
+if not exist %msbuildPath% (
+set msbuildPath="C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\MSBuild\15.0\Bin\msbuild.exe"
+)
+if not exist %msbuildPath% (
+echo Cound not find the Windows compiler MSBuild.exe.
+exit /b
+)
 
-"%msbuildPath%\msbuild.exe" /target:%buildConfig% /p:configuration=Debug  %~dp0%targetProgram%.sln
+%msbuildPath% /target:%buildConfig% /p:configuration=Debug  %~dp0%targetProgram%.sln
 
-"%msbuildPath%\msbuild.exe" /target:%buildConfig% /p:configuration=Release %~dp0%targetProgram%.sln
+%msbuildPath% /target:%buildConfig% /p:configuration=Release %~dp0%targetProgram%.sln
 
 if "%1"=="" copy /Y ..\..\..\exe\win\release\%targetProgram%.exe ..\..\..\..\..\win\
