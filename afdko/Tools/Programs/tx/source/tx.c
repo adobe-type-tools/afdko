28b25bd7283bb2b03484f0883ce54b0d19178348
Miguel Sousa <msousa@adobe.com>
2018-06-17 00:44:01 -0700

    [tx] bump version

--- afdko/Tools/Programs/tx/source/tx.c
+++ afdko/Tools/Programs/tx/source/tx.c
@@ -7,7 +7,7 @@ This software is licensed as OpenSource, under the Apache License, Version 2.0. 
 
 #include "ctlshare.h"
 
-#define TX_VERSION CTL_MAKE_VERSION(1,0,72)
+#define TX_VERSION CTL_MAKE_VERSION(1,1,0)
 
 #include "cfembed.h"
 #include "cffread.h"
