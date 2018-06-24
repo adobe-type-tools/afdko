9e25edbb4c3ce89bb7d2148c7113f3c769959df6
Miguel Sousa <msousa@adobe.com>
2018-06-24 15:23:30 -0700

    checkoutlinesufo integration tests

--- Tests/checkoutlinesufo_test.py
+++ Tests/checkoutlinesufo_test.py
@@ -1,10 +1,33 @@
 from __future__ import print_function, division, absolute_import
 
+import os
+import pytest
+from shutil import copytree
+import tempfile
+
 from booleanOperations.booleanGlyph import BooleanGlyph
 from defcon import Glyph
 
 from afdko.Tools.SharedData.FDKScripts.CheckOutlinesUFO import (
     remove_tiny_sub_paths)
+
+from .runner import main as runner
+from .differ import main as differ
+
+TOOL = 'checkoutlinesufo'
+CMD = ['-t', TOOL]
+
+UFO2_NAME = 'font.ufo'
+
+data_dir_path = os.path.join(os.path.split(__file__)[0], TOOL + '_data')
+
+
+def _get_expected_path(file_name):
+    return os.path.join(data_dir_path, 'expected_output', file_name)
+
+
+def _get_input_path(file_name):
+    return os.path.join(data_dir_path, 'input', file_name)
 
 
 # -----
@@ -37,3 +60,15 @@ def test_remove_tiny_sub_paths_small_contour():
     assert remove_tiny_sub_paths(bg, 25, []) == \
         ['Contour 0 is too small: bounding box is less than minimum area. '
          'Start point: ((1, 1)).']
+
+
+@pytest.mark.parametrize('args, expct_filename', [
+    (['e', 'w', 'q'], 'dflt_layer.ufo'),
+    (['e', 'q'], 'proc_layer.ufo'),
+])
+def test_remove_overlap(args, expct_filename):
+    actual_path = os.path.join(tempfile.mkdtemp(), UFO2_NAME)
+    copytree(_get_input_path(UFO2_NAME), actual_path)
+    runner(CMD + ['-n', '-f', actual_path, '-o'] + args)
+    expected_path = _get_expected_path(expct_filename)
+    assert differ([expected_path, actual_path])
