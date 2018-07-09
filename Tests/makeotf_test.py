adobe-type-tools/afdko

commit 35783e402a4d91263b1021ee677388739d4e7a4f

read files and decode

afdko/Tools/SharedData/FDKScripts/FDKUtils.py
@@ -74,7 +74,7 @@ def runShellCmd(cmd):
 	try:
 		p = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT).stdout
 		log = p.read()
-		return str(log)
+		return log.decode("latin-1")
 	except :
 		msg = "Error executing command '%s'. %s" % (cmd, traceback.print_exc())
 		print(msg)

afdko/Tools/SharedData/FDKScripts/MakeOTF.py
@@ -1394,8 +1394,12 @@ def getROS(fontPath):
 	"""
 	R=O=S=None
 	fp = open(fontPath, "rb")
-	data = str(fp.read(5000))
+	data = fp.read(5000)
 	fp.close()
+	try:
+		data = data.decode("latin-1")
+	except:
+		return R,O,S
 	match = re.search(r"/Registry\s+\((\S+)\)", data)
 	if not match:
 		return R,O,S

commit f43f983a8b376ca20e14d77bd9b12b7ee338a680

[fdkutils] Don't decode the log data because it leads to a UnicodeDecodeError later on.
Bug introduced in https://github.com/adobe-type-tools/afdko/commit/35783e402a4d91263b1021ee677388739d4e7a4f#diff-12c55c7004f8c76ee2b1741243a67cda

Using decode here brings back issue #222 (UnicodeDecodeError: 'ascii' codec can't decode byte).
Cosimo's patch (https://github.com/adobe-type-tools/afdko/issues/222#issuecomment-352723328) was included in https://github.com/adobe-type-tools/afdko/commit/f1c4511eaf6ae99fe784c516f16a3b8ae6a995c9#diff-8c875fc8969988dc6583127ac4fb12f7

afdko/Tools/SharedData/FDKScripts/FDKUtils.py
@@ -83,7 +83,7 @@ def runShellCmd(cmd):
         p = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE,
                              stderr=subprocess.STDOUT).stdout
         log = p.read()
-        return log.decode("latin-1")
+        return log
     except (OSError, ValueError):
         msg = "Error executing command '%s'. %s" % (cmd, traceback.print_exc())
         print(msg)

Tests/makeotf_test.py
@@ -1,9 +1,11 @@
+# coding: utf-8
+
 from __future__ import print_function, division, absolute_import
 
 import os
 import platform
 import pytest
-from shutil import copytree
+from shutil import copy2, copytree
 import subprocess32 as subprocess
 import tempfile
 
@@ -88,13 +90,30 @@ def test_input_formats(arg, input_filename, otf_filename):
                    '    <modified value='])
 
 
+@pytest.mark.parametrize('input_filename', [T1PFA_NAME, UFO2_NAME])
+def test_path_with_non_ascii_chars_bug222(input_filename):
+    temp_dir = os.path.join(tempfile.mkdtemp(), 'á意ê  ï薨õ 巽ù')
+    os.makedirs(temp_dir)
+    assert os.path.isdir(temp_dir)
+    input_path = _get_input_path(input_filename)
+    temp_path = os.path.join(temp_dir, input_filename)
+    if os.path.isdir(input_path):
+        copytree(input_path, temp_path)
+    else:
+        copy2(input_path, temp_path)
+    expected_path = os.path.join(temp_dir, 'SourceSansPro-Regular.otf')
+    assert os.path.exists(expected_path) is False
+    runner(CMD + ['-n', '-o', 'f', '_{}'.format(temp_path)])
+    assert os.path.isfile(expected_path)
+
+
 def test_ufo_with_trailing_slash_bug280():
     # makeotf will now save the OTF alongside the UFO instead of inside of it
     ufo_path = _get_input_path(UFO2_NAME)
     temp_dir = tempfile.mkdtemp()
     tmp_ufo_path = os.path.join(temp_dir, UFO2_NAME)
     copytree(ufo_path, tmp_ufo_path)
-    runner(CMD + ['-n', '-a', '-o', 'f', '_{}{}'.format(tmp_ufo_path, os.sep)])
+    runner(CMD + ['-n', '-o', 'f', '_{}{}'.format(tmp_ufo_path, os.sep)])
     expected_path = os.path.join(temp_dir, 'SourceSansPro-Regular.otf')
     assert os.path.isfile(expected_path)
 
@@ -106,8 +125,8 @@ def test_output_is_folder_only_bug281(input_filename):
     temp_dir = tempfile.mkdtemp()
     expected_path = os.path.join(temp_dir, 'SourceSansPro-Regular.otf')
     assert os.path.exists(expected_path) is False
-    runner(CMD + ['-n', '-a', '-o', 'f', '_{}'.format(input_path),
-                                    'o', '_{}'.format(temp_dir)])
+    runner(CMD + ['-n', '-o', 'f', '_{}'.format(input_path),
+                              'o', '_{}'.format(temp_dir)])
     assert os.path.isfile(expected_path)
 
 