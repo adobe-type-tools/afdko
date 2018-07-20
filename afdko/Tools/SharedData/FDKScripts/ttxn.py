5d5c32bee8b31f5355545741fd44f1c7ad5f6b3a
Miguel Sousa <msousa@adobe.com>
2018-07-20 01:26:33 -0700

    [ttxn] Fix GPOS Lookuptype 7, Format 2

--- afdko/Tools/SharedData/FDKScripts/ttxn.py
+++ afdko/Tools/SharedData/FDKScripts/ttxn.py
@@ -725,31 +725,27 @@ def ruleContextPOS(subtable, otlConv, context=None):
 
     elif subtable.Format == 2:
         subRules = []
-        for i in range(len(subtable.PosClassSet)):
-            ctxClassSet = subtable.PosClassSet[i]
+        for i, ctxClassSet in enumerate(subtable.PosClassSet):
             if not ctxClassSet:
                 continue
-            for j in range(len(ctxClassSet.PosClassRule)):
-                ctxClassRule = ctxClassSet.PosClassRule[j]
 
-                inputList = []
-                className = otlConv.classesByLookup[otlConv.curLookupIndex,
-                                                    otlConv.curSubTableIndex,
-                                                    i, otlConv.InputTag]
+            for ctxClassRule in ctxClassSet.PosClassRule:
+                className = otlConv.classesByLookup[
+                    otlConv.curLookupIndex, otlConv.curSubTableIndex, i, None]
                 inputList = [className]
                 inputList2 = [otlConv.classesByClassName[className]]
-                for c in range(len(ctxClassRule.Input)):
-                    classIndex = ctxClassRule.Input[c]
+
+                for classIndex in ctxClassRule.Class:
                     className = otlConv.classesByLookup[
                         otlConv.curLookupIndex, otlConv.curSubTableIndex,
-                        classIndex, otlConv.InputTag]
+                        classIndex, None]
                     inputList.append(className)
                     inputList2.append(otlConv.classesByClassName[className])
                 inputTxt = "' ".join(inputList) + "'"
                 rule = "pos %s;" % (inputTxt)
+
                 subRules = []
-                for k in range(len(ctxClassRule.PosLookupRecord)):
-                    subsRec = ctxClassRule.PosLookupRecord[k]
+                for subsRec in ctxClassRule.PosLookupRecord:
                     if not subsRec:
                         continue
                     lookup = pLookupList[subsRec.LookupListIndex]
@@ -757,15 +753,16 @@ def ruleContextPOS(subtable, otlConv, context=None):
                     curLI = otlConv.curLookupIndex
                     otlConv.curLookupIndex = subsRec.LookupListIndex
                     handler = otlConv.ruleHandlers[(lookupType)]
-                    contextRec = ContextRecord(inputList,
-                                               subsRec.SequenceIndex)
-                    for si in range(len(lookup.SubTable)):
+                    contextRec = ContextRecord(
+                        inputList, subsRec.SequenceIndex)
+
+                    for si, sub_table in enumerate(lookup.SubTable):
                         curSI = otlConv.curSubTableIndex
                         otlConv.curSubTableIndex = si
-                        subtablerules = handler(lookup.SubTable[si], otlConv,
-                                                contextRec)
+                        subtablerules = handler(sub_table, otlConv, contextRec)
                         otlConv.curSubTableIndex = curSI
                         subRules.extend(subtablerules)
+
                     otlConv.curLookupIndex = curLI
                 chainRules.append([rule, subRules])
 
