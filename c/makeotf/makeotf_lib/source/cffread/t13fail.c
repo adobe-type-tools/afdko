95d5d0263764affea0a038dc44d8101f623861c5
Christopher Chapman <cchapman@adobe.com>
2018-08-03 15:02:00 -0700

    reformatted makeotf's cffread code using clang-format

--- c/makeotf/makeotf_lib/source/cffread/t13fail.c
+++ c/makeotf/makeotf_lib/source/cffread/t13fail.c
@@ -1,5 +1,6 @@
 /* Copyright 2014 Adobe Systems Incorporated (http://www.adobe.com/). All Rights Reserved.
-   This software is licensed as OpenSource, under the Apache License, Version 2.0. This license is available at: http://opensource.org/licenses/Apache-2.0. *//***********************************************************************/
+   This software is licensed as OpenSource, under the Apache License, Version 2.0. This license is available at: http://opensource.org/licenses/Apache-2.0. */
+/***********************************************************************/
 
 /*
  * Dummy Type 13 module for implementations that don't support Morisawa fonts
@@ -10,5 +11,5 @@ static void t13Read(cffCtx h, Offset offset, int init) {
 }
 
 static int t13Support(cffCtx h) {
-	return 0;
+    return 0;
 }
