8aff8c09bedb56769a44915e3692ad8052ff9f2d
Miguel Sousa <msousa@adobe.com>
2018-08-07 23:07:26 -0700

    Revert "Update Visual Studio files to version 2017"
    This reverts commit 124563a1f965a3c763c8245b5c88bd63a50db482.

---
+++ c/tx/build/win/vc10/BuildAll.cmd
@@ -0,0 +1,37 @@
+cd %~dp0
+setlocal enabledelayedexpansion
+set targetProgram=tx
+set VCPATH="C:/Windows/Microsoft.NET/Framework/v4.0.30319/msbuild.exe"
+
+set do_release=0
+set do_target=0
+
+if "%1"=="" set do_release=1
+
+if "%1"=="release" set do_release=1
+
+if %do_release%==1 (
+	set buildConfig=Build
+	%VCPATH% /target:!buildConfig! /p:configuration=Release %~dp0%targetProgram%.sln
+	set do_target=1
+)
+
+if "%1"=="debug" (
+	set buildConfig=Build
+	%VCPATH% /target:!buildConfig! /p:configuration=Debug  %~dp0%targetProgram%.sln
+	set do_target=1
+)
+
+if %do_target%==1 (
+	copy /Y ..\..\..\exe\win\release\%targetProgram%.exe ..\..\..\..\build_all
+)
+
+if "%1"=="clean" (
+	set buildConfig=Clean
+	%VCPATH% /target:!buildConfig! /p:configuration=Debug  %~dp0%targetProgram%.sln
+	%VCPATH% /target:!buildConfig! /p:configuration=Release %~dp0%targetProgram%.sln
+	set do_target=1
+)
+if %do_target%==0 (
+	echo "Build target must be 'release', 'debug', 'clean', or simply omitted (same as 'release')" 
+)
