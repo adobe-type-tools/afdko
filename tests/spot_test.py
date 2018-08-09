812f2d794f34eef3f52c648da9af73a743e7d41a
ReadRoberts <rroberts@adobe.com>
2018-08-09 10:51:01 -0700

    [spot] name table difference between Mac/Win spot issue 468
    Remove test function in spot_test.py to verify that the test case fails under Windows and passes on the Mac, and restore the test case to the parameters for spot_test.py::test_options().
    
    spot currently sorts the name records by offset to the string, rather than name record order. If the offsets for two different names are the same, then the qsort is unstable, and Windows sorts differently than Mac. The fix is  to NOT sort, and report the records by name record order.
    
    The logic was originally an optimization for name table dumps other than level 3, as report of a name string could be omitted if it the name strings were sorted by offset, and the offset of the current record was the same as the previous record. However, I now think it more valuable to have one string dump per name record, and for the strings to be reported in name record order.

--- tests/spot_test.py
+++ tests/spot_test.py
@@ -51,10 +51,7 @@ def _get_expected_path(file_name):
     (['t', '_hmtx=7'], 'dump_hmtx=7.txt'),
     (['t', '_hmtx=8'], 'dump_hmtx=8.txt'),
     (['t', '_name=2'], 'dump_name=2.txt'),
-    # Fails on Windows, passes on Mac
-    # https://ci.appveyor.com/project/adobe-type-tools/afdko/build/1.0.217
-    # https://travis-ci.org/adobe-type-tools/afdko/builds/378601819
-    # (['t', '_name=3'], 'dump_name=3.txt'),
+    (['t', '_name=3'], 'dump_name=3.txt'),
     (['t', '_name=4'], 'dump_name=4.txt'),
     (['t', '_CFF_=6'], 'dump_CFF_=6.ps'),
     (['t', '_CFF_=6', 'a'], 'dump_CFF_=6a.ps'),
@@ -102,27 +99,3 @@ def test_bug465():
     actual_path = runner(CMD + ['-r', '-o', 't', '_GPOS=7', '-f', file_name])
     expected_path = _get_expected_path('bug465_otf.txt')
     assert differ([expected_path, actual_path])
-
-
-@pytest.mark.parametrize(
-    'args, exp_filename', [(['t', '_name=3'], 'dump_name=3.txt')])
-def test_bug468(args, exp_filename):
-    """ verify that with current code, this test fails on Windows. Using
-    the same logic as test_options(). spot currently sorts the name
-    records by offset to the string, rather than name record order. If
-    the offsets for two different names are the same, then the qsort is
-    unstable, and Windows sorts differently than Mac. The fix is to NOT
-    sort, and report the records by name record order. """
-
-    import platform
-    platform_system = platform.system()
-    if platform_system == "Windows":
-        dump_differs = True
-    elif platform_system == "Darwin":
-        dump_differs = False
-    else:
-        assert 0, "Test does not support " + platform_system
-
-    actual_path = runner(CMD + ['-r', '-f', 'black.otf', '-o'] + args)
-    expected_path = _get_expected_path(exp_filename)
-    assert (not dump_differs) == differ([expected_path, actual_path])
