57fcd3ca91840b5b76250f1a783facf99bdde369
Christopher Chapman <cchapman@adobe.com>
2018-08-23 15:52:59 -0700

    cleaned up issues reported by shellcheck (the tool used by Codacy to analyze shell scripts)

--- c/makeotf/build/osx/xcode4/BuildAll.sh
+++ c/makeotf/build/osx/xcode4/BuildAll.sh
@@ -4,18 +4,18 @@ set -x
 
 target=makeotf
 
-if [ -z "$1" ] || [ "$1" == "release" ]
+if [ -z "$1" ] || [ "$1" = "release" ]
 then
 	xcodebuild -target BuildAll -project $target.xcodeproj -configuration Release
 	cp ../../../exe/osx/release/${target}exe ../../../../build_all
-elif [ "$1" == "debug" ]
+elif [ "$1" = "debug" ]
 then
 	xcodebuild -target BuildAll -project $target.xcodeproj -configuration Debug
 	cp ../../../exe/osx/debug/${target}exe ../../../../build_all
-elif [ "$1" == "clean" ]
+elif [ "$1" = "clean" ]
 then
-	xcodebuild -target BuildAll -project $target.xcodeproj -configuration Debug $1
-	xcodebuild -target BuildAll -project $target.xcodeproj -configuration Release $1
+	xcodebuild -target BuildAll -project $target.xcodeproj -configuration Debug "$1"
+	xcodebuild -target BuildAll -project $target.xcodeproj -configuration Release "$1"
 else
    echo "Build target must be 'release', 'debug', 'clean', or simply omitted (same as 'release')"
 fi
