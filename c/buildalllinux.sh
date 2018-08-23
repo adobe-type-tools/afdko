14e2e0318bb16ef4649d6ebb3117891784c96533
Christopher Chapman <cchapman@adobe.com>
2018-08-23 15:52:59 -0700

    cleaned up issues reported by shellcheck (the tool used by Codacy to analyze shell scripts)

--- c/buildalllinux.sh
+++ c/buildalllinux.sh
@@ -1,23 +1,25 @@
+#! /bin/sh
+
 # How to find and add all the xcode files for a new program:
 # find .   \( -path "*/xcode4/*" \)  -and \( \(  -name BuildAll.sh \) -or \( -name project.pbxproj \) -or \( -name contents.xcworkspacedata  \) \)  -exec p4 add {} \;curDir=`pwd`
 
 set -e
 set -x
 
-curDir=`pwd`
+curDir=$(pwd)
 
 if [ -z "$1" ] || [ "$1" = "release" ] || [ "$1" = "debug" ] || [ "$1" = "clean" ]
 then
-	buildAllList=`ls -1 */build/linux/gcc/BuildAll.sh`
+	buildAllList=$(ls -1 ./*/build/linux/gcc/BuildAll.sh)
 	for shFile in $buildAllList
 	do
 		echo "***Running $shFile"
-		newDir=`dirname $shFile`
-		shFile=`basename $shFile`
-		cd $newDir
-		sh	$shFile $1
+		newDir=$(dirname "$shFile")
+		shFile=$(basename "$shFile")
+		cd "$newDir"
+		sh	"$shFile" "$1"
 		failed=$?
-		cd $curDir
+		cd "$curDir"
 		if [ "$failed" -ne 0 ]; then
 			exit "$failed"
 		fi
--- c/tx/build/linux/gcc/BuildAll.sh
+++ c/tx/build/linux/gcc/BuildAll.sh
@@ -3,28 +3,28 @@ set -e
 set -x
 
 target=tx
-curdir=`pwd`
+curdir=$(pwd)
 
-if [ -z "$1" ] || [ $1 = "release" ]
+if [ -z "$1" ] || [ "$1" = "release" ]
 then
 	cd release
 	make
-	cd $curdir
+	cd "$curdir"
 	cp -dR ../../../exe/linux/release/$target ../../../../build_all
-elif [ $1 = "debug" ]
+elif [ "$1" = "debug" ]
 then
 	cd debug
 	make
-	cd $curdir
+	cd "$curdir"
 	cp -dR ../../../exe/linux/debug/$target ../../../../build_all
-elif [ $1 = "clean" ]
+elif [ "$1" = "clean" ]
 then
 	cd release
-	make $1
-	cd $curdir
+	make "$1"
+	cd "$curdir"
 	cd debug
-	make $1
-	cd $curdir
+	make "$1"
+	cd "$curdir"
 else
    echo "Build target must be 'release', 'debug', 'clean', or simply omitted (same as 'release')"
 fi
