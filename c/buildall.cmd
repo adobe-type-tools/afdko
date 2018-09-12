9aa4ebe29f5c44bda07bfb17cd33157342177422
Miguel Sousa <msousa@adobe.com>
2018-09-11 23:45:42 -0700

    Rename Windows build files
    It's a misnomer and totally confusing to name these things 'BuildAll'

--- c/buildall.cmd
+++ c/buildall.cmd
@@ -11,38 +11,38 @@ if %do_target%==0 (
 
 echo " "
 echo "Building detype1..."
-call %~dp0detype1\build\win\visualstudio\BuildAll.cmd %1 || EXIT /B 1
+call %~dp0detype1\build\win\visualstudio\build.cmd %1 || EXIT /B 1
 
 echo " "
 echo "Building makeotf..."
-call %~dp0makeotf\build\win\visualstudio\BuildAll.cmd %1 || EXIT /B 1
+call %~dp0makeotf\build\win\visualstudio\build.cmd %1 || EXIT /B 1
 
 echo " "
 echo "Building mergefonts..."
-call %~dp0mergefonts\build\win\visualstudio\BuildAll.cmd %1 || EXIT /B 1
+call %~dp0mergefonts\build\win\visualstudio\build.cmd %1 || EXIT /B 1
 
 echo " "
 echo "Building rotatefont..."
-call %~dp0rotatefont\build\win\visualstudio\BuildAll.cmd %1 || EXIT /B 1
+call %~dp0rotatefont\build\win\visualstudio\build.cmd %1 || EXIT /B 1
 
 echo " "
 echo "Building sfntdiff..."
-call %~dp0sfntdiff\build\win\visualstudio\BuildAll.cmd %1 || EXIT /B 1
+call %~dp0sfntdiff\build\win\visualstudio\build.cmd %1 || EXIT /B 1
 
 echo " "
 echo "Building sfntedit..."
-call %~dp0sfntedit\build\win\visualstudio\BuildAll.cmd %1 || EXIT /B 1
+call %~dp0sfntedit\build\win\visualstudio\build.cmd %1 || EXIT /B 1
 
 echo " "
 echo "Building spot..."
-call %~dp0spot\build\win\visualstudio\BuildAll.cmd %1 || EXIT /B 1
-
-echo " "
-echo "Building type1..."
-call %~dp0type1\build\win\visualstudio\BuildAll.cmd %1 || EXIT /B 1
+call %~dp0spot\build\win\visualstudio\build.cmd %1 || EXIT /B 1
 
 echo " "
 echo "Building tx..."
-call %~dp0tx\build\win\visualstudio\BuildAll.cmd %1 || EXIT /B 1
+call %~dp0tx\build\win\visualstudio\build.cmd %1 || EXIT /B 1
+
+echo " "
+echo "Building type1..."
+call %~dp0type1\build\win\visualstudio\build.cmd %1 || EXIT /B 1
 
 cd %~dp0
