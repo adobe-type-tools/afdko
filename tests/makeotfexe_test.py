9e1273947379f1b3db9c087a53882541bc0f4230
ReadRoberts <rroberts@adobe.com>
2018-09-26 15:59:07 -0700

    [makeotfexe test] Tests for GSUB and GPOS warnings
    Added tests for all the GPOS.c and GSUB.c warnings other than coverage overflow. I needed to do this because I touched all of these in order to add the parent feature name to the error report, just as I did with the coverage overflow errors.
    
    Note that I did not do this with the warnings in feat.c. This is because the warnings in feat.c are generated during parsing ,and consequently have access to and use the parent feature file name and line number, which is better pointer to the source of a problem than the parent feature name. These are usually not available when the GPOS and GSUB structures are being filled and written, and then displaying the parent feature name is more important.

--- tests/makeotfexe_test.py
+++ tests/makeotfexe_test.py
@@ -179,8 +179,103 @@ def test_version_warning_bug610():
         b"[FATAL] <SourceSans-Test> In feature 'rts1' lookup 'lkup238' "
         b"reverse chain contextual substitution rules cause an offset "
         b"overflow (0x100a0) to a lookup subtable"),
+    ('test_duplicate_single_sub',
+        b"[NOTE] <SourceSans-Test> Removing duplicate single substitution "
+        b"in standalone lookup 'test2': glyph1, glyph1"),
+    ('test_conflicting_single_sub',
+        b"[FATAL] <SourceSans-Test> Duplicate target glyph for single "
+        b"substitution in standalone lookup 'test2': glyph1"),
+    ('test_duplicate_alternate_sub',
+        b"[FATAL] <SourceSans-Test> Duplicate target glyph for alternate "
+        b"substitution in standalone lookup 'test2': glyph1"),
+    ('test_duplicate_multiple_sub',
+        b"[FATAL] <SourceSans-Test> Duplicate target glyph for multiple "
+        b"substitution in standalone lookup 'test2': glyph1"),
+    ('test_duplicate_ligature_sub',
+        b"[NOTE] <SourceSans-Test> Removing duplicate ligature substitution "
+        b"in standalone lookup 'test2'"),
+    ('test_conflicting_ligature_sub',
+        b"[FATAL] <SourceSans-Test> Duplicate target sequence but different "
+        b"replacement glyphs in ligature substitutions in standalone "
+        b"lookup 'test2'"),
+    ('test_duplicate_single_pos',
+        b"[NOTE] <SourceSans-Test> Removing duplicate single positioning "
+        b"in standalone lookup 'test2'"),
+    ('test_conflicting_single_pos',
+        b"[FATAL] <SourceSans-Test> Duplicate single positioning glyph "
+        b"with different values in standalone lookup 'test2'"),
+    ('test_features_name_missing_win_dflt_sub',
+        b"[FATAL] <SourceSans-Test> Missing Windows default name for "
+        b"'featureNames' nameid 256 in feature 'ss01'"),
+    ('test_cv_params_not_in_cvxx_sub',
+        b"[FATAL] <SourceSans-Test> A 'cvParameters' block is only allowed "
+        b"in Character Variant (cvXX) features; it is being used in "
+        b"feature 'tst1'"),
+    ('test_cv_params_missing_win_dflt_sub',
+        b"[FATAL] <SourceSans-Test> Missing Windows default name for "
+        b"'cvParameters' nameid 256 in feature 'cv01'"),
+    ('test_size_withfamilyID_0_pos',
+        b"[FATAL] <SourceSans-Test> 'size' feature must have 4 parameters if "
+        b"sub family ID code is non-zero!"),
+    ('test_size_withfamilyID_3_pos',
+        b"[FATAL] <SourceSans-Test> 'size' feature must have 4 or 2 "
+        b"parameters if sub family code is zero!"),
+    ('test_sizemenuname_missing_win_dflt_pos',
+        b"[FATAL] <SourceSans-Test> Missing Windows default name for "
+        b"'sizemenuname' nameid 256 in 'size' feature"),
+    ('test_kernpair_warnings_pos',
+        b"[WARNING] <SourceSans-Test> Single kern pair occurring after class "
+        b"kern pair in feature 'tst1'"),
+    ('test_kernpair_warnings_pos',
+        b"[WARNING] <SourceSans-Test> Start of new pair positioning subtable "
+        b"forced by overlapping glyph classes in feature 'tst1'; some pairs "
+        b"may never be accessed"),
+    ('test_kernpair_warnings_pos',
+        b"[NOTE] <SourceSans-Test> Removing duplicate pair positioning in "
+        b"feature 'tst1': glyph3 glyph3"),
+    ('test_kernpair_warnings_pos',
+        b"[WARNING] <SourceSans-Test> Pair positioning has conflicting "
+        b"statements in feature 'tst1'"),
+    ('test_base_glyph_conflict_pos',
+        b"[ERROR] <SourceSans-Test> MarkToBase or MarkToMark error in "
+        b"feature 'tst1'. Another statement has already defined the anchors "
+        b"and marks on glyph 'glyph5'."),
+    ('test_base_glyph_conflict_pos',
+        b"[WARNING] <SourceSans-Test> MarkToBase or MarkToMark error in "
+        b"feature 'tst1'. Glyph 'glyph5' does not have an anchor point "
+        b"for a mark class that was used in a previous statement in the "
+        b"same lookup table. Setting the anchor point offset to 0."),
+    ('test_mark_ligature_conflict_pos',
+        b"[ERROR] <SourceSans-Test> MarkToLigature error in feature 'tst1'. "
+        b"Two different statements referencing the ligature glyph 'glyph6' "
+        b"have assigned the same mark class to the same ligature component."),
+    ('test_mark_ligature_conflict_pos',
+        b"[ERROR] <SourceSans-Test> MarkToLigature statement error in feature "
+        b"'tst1'. Glyph 'glyph6' contains a duplicate mark class assignment "
+        b"for one of the ligature components."),
+    ('test_mark_class_glyph_conflict',
+        b"[ERROR] <SourceSans-Test> In feature 'tst1', glyph 'glyph2' "
+        b"is repeated in the current class definition. Mark class: "
+        b"@TOP_MARKS."),
+    ('test_mark_class_glyph_conflict',
+        b"[ERROR] <SourceSans-Test> In feature 'tst1', glyph 'glyph1' "
+        b"occurs in two different mark classes. Previous mark class: "
+        b"@TOP_MARKS. Current mark class: @BOTTOM_MARKS."),
+    ('test_base_anchor_errors_pos',
+        b"[ERROR] <SourceSans-Test> MarkToBase or MarkToMark error in "
+        b"feature 'tst1'. Another statement has already assigned the current "
+        b"mark class to another anchor point on glyph 'glyph7'"),
+    ('test_base_anchor_errors_pos',
+        b"[WARNING] <SourceSans-Test> MarkToBase or MarkToMark error in "
+        b"feature 'tst1'. Glyph 'glyph5' does not have an anchor point "
+        b"for a mark class that was used in a previous statement in the "
+        b"same lookup table"),
+    ('test_cursive_duplicate_glyph_pos',
+        b"[ERROR] <SourceSans-Test> Cursive statement error in feature "
+        b"'tst1'. A previous statement has already referenced glyph "
+        b"'glyph1'."),
 ])
-def test_oveflow_report_bug313(feat_name, error_msg):
+def test_overflow_report_bug313(feat_name, error_msg):
     input_filename = 'bug313/font.pfa'
     feat_filename = 'bug313/{}.fea'.format(feat_name)
     otf_path = get_temp_file_path()
