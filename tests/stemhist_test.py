c6d6758c7239066aa47d2bdff879d96ab41b2864
Miguel Sousa <msousa@adobe.com>
2019-02-13 02:32:02 -0800

    Test case for #210

--- tests/stemhist_test.py
+++ tests/stemhist_test.py
@@ -45,3 +45,19 @@ def test_stems_and_zones(arg, font_filename):
             exp_suffix = '.all' + suffix
         expected_path = get_expected_path('{}{}'.format(prefix, exp_suffix))
         assert differ([expected_path, actual_path, '-l', '1'])
+
+
+def test_cross_environment_results_bug210():
+    filename = 'bug210'
+    report_path = get_temp_file_path()
+    actual_top_path = '{}.top.txt'.format(report_path)
+    actual_bot_path = '{}.bot.txt'.format(report_path)
+    expect_top_path = get_expected_path('{}.top.txt'.format(filename))
+    expect_bot_path = get_expected_path('{}.bot.txt'.format(filename))
+
+    runner(CMD + ['-f', '{}.ufo'.format(filename),
+                  '-o', 'a', 'g', '_a-z,A-Z,zero-nine',
+                  'o', '_{}'.format(report_path)])
+
+    assert differ([expect_top_path, actual_top_path, '-l', '1'])
+    assert differ([expect_bot_path, actual_bot_path, '-l', '1'])
