d5738078dd604b0566d208dac301015a45922542
Miguel Sousa <msousa@adobe.com>
2019-03-15 15:40:55 -0700

    CircleCI enhancements [skip travis] [skip appveyor]
    The changes are:
    * Simplify the config (CircleCI does not support global environment variables, and the python setup for Linux is different from the Mac, so there's no advantage in using sharable commands)
    * Trigger builds on tags (The default is only commits)
    * Don't do static code analysis on tags
    * Enable Mac job (This job requires a fork of cibuildwheel that has pinned versions of wheel and delocate; without this pinning the C-tools will not get configured as executables)

--- .circleci/setup_linux_pyenv.sh
+++ .circleci/setup_linux_pyenv.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 # create virtual environment
-python -m venv venv
+$PYTHON -m venv venv
 # activate virtual environment
 source venv/bin/activate
 # install and update dependencies
-pip install -U -q pip setuptools wheel flake8 cpplint
-pip install cibuildwheel
+$PIP install -U -q pip setuptools wheel flake8 cpplint
+$PIP install cibuildwheel
 # print versions
-python --version
-pip --version
-pip list
+$PYTHON --version
+$PIP --version
+$PIP list
---
+++ .circleci/setup_mac_pyenv.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# create virtual environment
+$PYTHON -m venv venv
+# activate virtual environment
+source venv/bin/activate
+# install and update dependencies
+$PIP install -U -q pip setuptools wheel
+$PIP install git+https://github.com/miguelsousa/cibuildwheel
+# print versions
+$PYTHON --version
+$PIP --version
+$PIP list
