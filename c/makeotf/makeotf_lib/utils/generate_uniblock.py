6e805c0988deb2abc874bc8466507fb576162d5c
Christopher Chapman <cchapman@adobe.com>
2019-06-20 16:31:19 -0700

    [makeotf] refactored generate_uniblock.py (to make Codacy/pylint happy)

--- c/makeotf/makeotf_lib/utils/generate_uniblock.py
+++ c/makeotf/makeotf_lib/utils/generate_uniblock.py
@@ -22,8 +22,8 @@ import csv
 
 def _count_chars_in_range(unichars, start, end):
     count = 0
-    for c in range(start, end + 1):
-        if c in unichars:
+    for unichar in range(start, end + 1):
+        if unichar in unichars:
             count += 1
     return count
 
@@ -31,9 +31,9 @@ def _count_chars_in_range(unichars, start, end):
 def _fix_title_casing(name):
     conjunctions = ['And', 'For']
     for conjunction in conjunctions:
-        x = ' ' + conjunction + ' '
-        if x in name:
-            name = name.replace(x, x.lower())
+        conj_with_spaces = ' ' + conjunction + ' '
+        if conj_with_spaces in name:
+            name = name.replace(conj_with_spaces, conj_with_spaces.lower())
     return name
 
 
@@ -62,28 +62,31 @@ def _print_header():
     )
 
 
-def _main():
-    _print_header()
+def _get_unichars():
     unichars = set()
     with open('UnicodeData.txt') as unicodedata_file:
         reader = csv.reader(unicodedata_file, delimiter=';')
         start = None
         for row in reader:
-            c = int(row[0], 16)
+            unichar = int(row[0], 16)
             name = row[1]
             if 'First' in name:
-                start = c
+                start = unichar
             elif 'Last' in name:
-                for x in range(start, c + 1):
-                    unichars.add(x)
+                for char_in_range in range(start, unichar + 1):
+                    unichars.add(char_in_range)
                 start = None
             else:
-                unichars.add(c)
+                unichars.add(unichar)
+    return unichars
 
+
+def _get_os2_ur_records():
     # we want to set the corresponding OS/2 Unicode range bits
     # if we see *any* character in a PUA or CJK block
     bits_to_flag_on_any_char = {28, 48, 52, 54, 55, 56, 59, 60, 61, 65, 90}
 
+    unichars = _get_unichars()
     records = list()
     prev_os2_num = 0
     with open('os2_ur.txt') as input_file:
@@ -106,6 +109,10 @@ def _main():
             record = (start, end, count, os2_num, name)
             records.append(record)
             prev_os2_num = os2_num
+    return records
+
+
+def _print_records(records):
     for record in sorted(records):
         (start, end, count, os2_num, name) = record
         start_str = '0x%04X' % start
@@ -113,6 +120,12 @@ def _main():
         name_str = '"%s"' % name
         print('    { %8s, %8s, %5d, %d, %3d, %d, %-41s },'
               % (start_str, end_str, count, 0, os2_num, 0, name_str))
+
+
+def _main():
+    _print_header()
+    records = _get_os2_ur_records()
+    _print_records(records)
 
 
 if __name__ == '__main__':
