64c72bf3920ef90ddc482fa950151fb02e446454
Miguel Sousa <msousa@adobe.com>
2019-08-11 21:40:28 -0700

    [checkoutlinesufo] Fix support for non-UFO font formats

--- tests/checkoutlinesufo_test.py
+++ tests/checkoutlinesufo_test.py
@@ -1,24 +1,25 @@
 import pytest
-from shutil import copytree
+from shutil import copy2, copytree
 
 from booleanOperations.booleanGlyph import BooleanGlyph
 from defcon import Glyph
 
 from afdko.checkoutlinesufo import remove_tiny_sub_paths
 
-from afdko.fdkutils import get_temp_dir_path
+from afdko.fdkutils import (
+    get_temp_file_path,
+    get_temp_dir_path,
+)
 from test_utils import (
     get_input_path,
     get_expected_path,
+    generate_ttx_dump,
 )
 from runner import main as runner
 from differ import main as differ
 
 TOOL = 'checkoutlinesufo'
 CMD = ['-t', TOOL]
-
-UFO2_NAME = 'ufo2.ufo'
-UFO3_NAME = 'ufo3.ufo'
 
 
 # -----
@@ -53,15 +54,36 @@ def test_remove_tiny_sub_paths_small_contour():
          'Start point: ((1, 1)).']
 
 
-@pytest.mark.parametrize('ufo_filename', [UFO2_NAME, UFO3_NAME])
+@pytest.mark.parametrize('ufo_filename', ['ufo2.ufo', 'ufo3.ufo'])
 @pytest.mark.parametrize('args, expct_label', [
     (['e', 'w', 'q'], 'dflt-layer.ufo'),
     (['e', 'q'], 'proc-layer.ufo'),
 ])
-def test_remove_overlap(args, ufo_filename, expct_label):
+def test_remove_overlap_ufo(args, ufo_filename, expct_label):
     actual_path = get_temp_dir_path(ufo_filename)
     copytree(get_input_path(ufo_filename), actual_path)
     runner(CMD + ['-f', actual_path, '-o'] + args)
     expct_filename = f'{ufo_filename[:-4]}-{expct_label}'
     expected_path = get_expected_path(expct_filename)
     assert differ([expected_path, actual_path])
+
+
+@pytest.mark.parametrize('filename, diffmode', [
+    ('font.pfa', []),
+    ('font.cff', ['-m', 'bin']),
+])
+def test_remove_overlap_type1_cff(filename, diffmode):
+    actual_path = get_temp_file_path()
+    copy2(get_input_path(filename), actual_path)
+    runner(CMD + ['-f', actual_path, '-o', 'e', 'q'])
+    expected_path = get_expected_path(filename)
+    assert differ([expected_path, actual_path] + diffmode)
+
+
+def test_remove_overlap_otf():
+    actual_path = get_temp_file_path()
+    copy2(get_input_path('font.otf'), actual_path)
+    runner(CMD + ['-f', actual_path, '-o', 'e', 'q'])
+    actual_ttx = generate_ttx_dump(actual_path, ['CFF '])
+    expected_ttx = get_expected_path('font.ttx')
+    assert differ([expected_ttx, actual_ttx, '-s', '<ttFont sfntVersion'])
