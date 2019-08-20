7b50c9a32c2580c54f0c5d0074232952c0c4073e
Miguel Sousa <msousa@adobe.com>
2019-08-20 14:51:50 -0700

    [ci] Move coverage run from Travis to Circle
    and don't use cibuildwheel

--- .circleci/setup_linux_pyenv.sh
+++ .circleci/setup_linux_pyenv.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
+set -e
+
 # create virtual environment
 $PYTHON -m venv venv
 # activate virtual environment
 source venv/bin/activate
 # install and update dependencies
-$PIP install -U -q pip setuptools wheel flake8 cpplint
-$PIP install cibuildwheel
+$PIP install --disable-pip-version-check -U -q pip setuptools wheel flake8 cpplint
+$PIP install pytest-cov codecov
 # print versions
 $PYTHON --version
 $PIP --version
