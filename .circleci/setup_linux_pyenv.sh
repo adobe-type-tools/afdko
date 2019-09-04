d5fafd7d9fe831d18ae001c078520fbbedd24ae0
Miguel Sousa <msousa@adobe.com>
2019-09-04 00:21:55 -0700

    [circleci] Minor tweaks [skip travis] [skip appveyor]

--- .circleci/setup_linux_pyenv.sh
+++ .circleci/setup_linux_pyenv.sh
@@ -7,8 +7,7 @@ $PYTHON -m venv venv
 # activate virtual environment
 source venv/bin/activate
 # install and update dependencies
-$PIP install --disable-pip-version-check -U -q pip setuptools wheel flake8 cpplint
-$PIP install pytest-cov codecov
+$PIP install --disable-pip-version-check -U -q pip setuptools wheel flake8 cpplint pytest-cov codecov
 # print versions
 $PYTHON --version
 $PIP --version
