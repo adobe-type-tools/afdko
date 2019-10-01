b3332071f11688107cc04a7c1cc0ffa866ad8380
Miguel Sousa <msousa@adobe.com>
2019-10-01 00:12:10 -0700

    Rewrite of otc2otf
    Now it leverages FontTools as much as possible.
    Option '-u' was eliminated and the formatting of the report was greatly improved.

--- tests/otc2otf_test.py
+++ tests/otc2otf_test.py
@@ -3,9 +3,14 @@ import pytest
 from shutil import copy2, rmtree
 import subprocess
 
+from afdko.otc2otf import get_options
+from test_utils import (
+    get_input_path,
+    get_expected_path,
+    generate_ttx_dump,
+)
 from runner import main as runner
-from differ import main as differ
-from test_utils import get_input_path, get_expected_path, generate_ttx_dump
+from differ import main as differ, SPLIT_MARKER
 
 TOOL = 'otc2otf'
 CMD = ['-t', TOOL]
@@ -39,27 +44,36 @@ def teardown_module():
 # Tests
 # -----
 
-@pytest.mark.parametrize('arg', ['-h', '-u'])
+@pytest.mark.parametrize('arg', ['-h'])
 def test_exit_known_option(arg):
     assert subprocess.call([TOOL, arg]) == 0
 
 
 @pytest.mark.parametrize('arg', ['-j', '--foobar'])
 def test_exit_unknown_option(arg):
-    assert subprocess.call([TOOL, arg]) == 0
+    assert subprocess.call([TOOL, arg]) == 2
 
 
 def test_exit_no_font_provided():
-    assert subprocess.call([TOOL]) == 0
+    assert subprocess.call([TOOL]) == 2
 
 
 def test_exit_invalid_path():
-    assert subprocess.call([TOOL, 'foobar']) == 0
+    assert subprocess.call([TOOL, 'foobar']) == 2
 
 
 def test_exit_not_ttc_font():
     font_path = get_input_path('font.ttf')
-    assert subprocess.call([TOOL, font_path]) == 0
+    assert subprocess.call([TOOL, font_path]) == 2
+
+
+@pytest.mark.parametrize('v_arg', ['', 'v', 'vv', 'vvv'])
+def test_log_level(v_arg):
+    in_path = get_input_path('RgIt.ttc')
+    v_val = len(v_arg)
+    arg = [] if not v_val else [f'-{v_arg}']
+    opts = get_options([in_path] + arg)
+    assert opts.verbose == v_val
 
 
 @pytest.mark.parametrize('ttc_filename, input_filenames, diff_index', [
@@ -76,14 +90,7 @@ def test_convert(ttc_filename, input_filenames, diff_index):
     temp_ttc_path = os.path.join(TEMP_DIR, ttc_filename)
     copy2(input_ttc_path, temp_ttc_path)
 
-    fonts_msg = os.linesep.join(
-        [f'Output font: {fname}' for fname in input_filenames])
-
-    stdout_path = runner(CMD + ['-s', '-a', '-f', temp_ttc_path])
-
-    with open(stdout_path, 'rb') as f:
-        output = f.read()
-    assert fonts_msg.encode('ascii') in output
+    runner(CMD + ['-a', '-f', temp_ttc_path])
 
     # diff only one of the OTFs
     input_filename = input_filenames[diff_index]
@@ -92,24 +99,32 @@ def test_convert(ttc_filename, input_filenames, diff_index):
     actual_ttx = generate_ttx_dump(actual_path)
     in_ext = os.path.splitext(input_filename)[1]
     expected_ttx = get_expected_path(input_filename.replace(in_ext, '.ttx'))
-    assert differ([expected_ttx, actual_ttx, '-s', '<ttFont sfntVersion'])
+    return differ([expected_ttx, actual_ttx,
+                   '-s',
+                   '<ttFont sfntVersion' + SPLIT_MARKER +
+                   '    <checkSumAdjustment value=' + SPLIT_MARKER +
+                   '    <created value=' + SPLIT_MARKER +
+                   '    <modified value='])
 
 
 @pytest.mark.parametrize('filename', ['RgBd.ttc', 'RgIt.ttc', 'RgItBd.ttc'])
 def test_report(filename):
-    ttc_path = get_input_path(filename)
     expected_report = get_expected_path(filename.replace('.ttc', '.txt'))
-
-    stdout_path = runner(CMD + ['-s', '-a', '-o', 'r', '-f', ttc_path])
-
-    assert differ([expected_report, stdout_path, '-l', '1'])
+    stdout_path = runner(CMD + ['-s', '-o', 'r', '-f', filename])
+    assert differ([expected_report, stdout_path])
 
 
 def test_psnames():
-    runner(CMD + ['-f', get_input_path('no_psname-mac_psname.ttc')])
-    fpath1 = get_input_path('PSNameUndefined.ttf')
+    runner(CMD + ['-f', 'no_psname-mac_psname-no_name_tb.ttc'])
+    fpath1 = get_input_path('no_psname-mac_psname-no_name_tb-font0.ttf')
     fpath2 = get_input_path('mac_psname.ttf')
+    fpath3 = get_input_path('no_psname-mac_psname-no_name_tb-font2.ttf')
 
-    for fpth in (fpath1, fpath2):
+    for fpth in (fpath1, fpath2, fpath3):
         assert os.path.exists(fpth)
         os.remove(fpth)
+
+
+def test_exception():
+    with pytest.raises(Exception):
+        runner(CMD + ['-o', 'r', '-f', 'invalid_num_fonts.ttc'])
