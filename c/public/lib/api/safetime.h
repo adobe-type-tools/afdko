71d9649bbb371ac47740aafad880bf05c00636e2
Christopher Chapman <cchapman@adobe.com>
2019-12-13 14:02:19 -0800

    [pdfwrite] fix use of potentially dangerous functions

--- c/public/lib/api/safetime.h
+++ c/public/lib/api/safetime.h
@@ -11,10 +11,12 @@ This license is available at: http://opensource.org/licenses/Apache-2.0.
 #define SAFE_LOCALTIME(x, y)    localtime_s(y, x)
 #define LOCALTIME_FAILURE(x)    (x != 0)
 #define SAFE_CTIME(x, y)        ctime_s(y, sizeof(y), x)
+#define SAFE_GMTIME(x, y)       gmtime_s(y, x)
 #else
 #define SAFE_LOCALTIME(x, y)    localtime_r(x, y)
 #define LOCALTIME_FAILURE(x)    (x == NULL)
 #define SAFE_CTIME(x, y)        ctime_r(x, y)
+#define SAFE_GMTIME(x, y)       gmtime_r(x, y)
 #endif /* WIN32 */
 
 #endif /* SAFETIME_H */
