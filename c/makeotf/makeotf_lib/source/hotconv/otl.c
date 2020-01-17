adobe-type-tools/afdko

commit 5624e02b58390e605639c3633025cd73cd418da7

[makeotfexe] Fix overflow in a Feature Record FeatureParam field, issue 1017. In rare cases - usually with many contextual rules - the Feature Record FeatureParam field can overflow because the FeatureParam block is written after the lookup subtables from the same feature, and the size of the LookupList alone can push the offset into overflow. See issue https://github.com/adobe-type-tools/afdko/issues/1017.

c/makeotf/makeotf_lib/source/hotconv/otl.c
@@ -265,6 +265,7 @@ struct otlTbl_ {
     short nStandAloneSubtables;
     short nRefLookups; /* number of otl Subtable 's that are only references to look ups*/
     short nFeatParams; /* number of otl Subtable 's that hold feature table parameters.*/
+    LOffset params_size; /* size of feature parameter subtable block.*/
 #if HOT_DEBUG
     int nCoverageReused;
     int nClassReused;
@@ -1654,7 +1655,7 @@ static Offset fillFeatureList(hotCtx g, otlTbl t) {
                 /* This happens when the 'size' feature is the first GPOS feature in the feature file.*/
                 nParamOffset = FEAT_PARAM_ZERO; /* So we can tell the difference between 'undefined' and a real zero value.*/
             }
-            feature->FeatureParams = nParamOffset; /* Note! this is only the offset from the start of the subtable block that follows the lookupList */
+            feature->FeatureParams = nParamOffset; /* Note! this is only the offset from the start of the featureParam block, which follows the FeatureList and records. */
         } else {
             feature->FeatureParams = NULL_OFFSET;
         }
@@ -1666,7 +1667,7 @@ static Offset fillFeatureList(hotCtx g, otlTbl t) {
     return oFeatureList;
 }
 
-static void fixFeatureParmOffsets(hotCtx g, otlTbl t, short shortfeatureParamBaseOffset) {
+static void fixFeatureParamOffsets(hotCtx g, otlTbl t, short shortfeatureParamBaseOffset) {
     int nFeatures = t->tbl.FeatureList_.FeatureCount;
     int i;
 
@@ -1679,8 +1680,8 @@ static void fixFeatureParmOffsets(hotCtx g, otlTbl t, short shortfeatureParamBas
             if (feature->FeatureParams == FEAT_PARAM_ZERO) {
                 feature->FeatureParams = 0;
             }
-            /* feature->FeatureParams is now: (offset from start of subtable block that follows the LookupList) */
-            /* shortfeatureParamBaseOffset is (size of featureList) + (size of LookupList).                     */
+            /* feature->FeatureParams is now: (offset from start of featureParams block that follows the FeatureList and feature records) */
+            /* featureParamBaseOffset is (size of featureList).                     */
             feature->FeatureParams = feature->FeatureParams + shortfeatureParamBaseOffset - rec->Feature;
             if (feature->FeatureParams > 0xFFFF) {
                 hotMsg(g, hotFATAL, "feature parameter offset too large (%0x)",
@@ -1768,8 +1769,10 @@ void checkStandAloneTablRefs(hotCtx g, otlTbl t) {
     }
 }
 
-void otlTableFill(hotCtx g, otlTbl t) {
+void otlTableFill(hotCtx g, otlTbl t, LOffset params_size) {
     Offset offset = HEADER_SIZE;
+    Offset size;
+    t->params_size = params_size;
 
     calcLookupListIndexes(g, t);
     calcFeatureListIndexes(g, t);
@@ -1788,18 +1791,20 @@ void otlTableFill(hotCtx g, otlTbl t) {
 
     prepFeatureList(g, t);
     t->tbl.FeatureList = offset;
-    offset += fillFeatureList(g, t);
+    size = fillFeatureList(g, t);
+    offset += size;
+    if (t->nFeatParams > 0) {
+        /* The feature table FeatureParam offsets are currently from the start of the featureParam
+        block, starting right after the FeatureList.
+        featureParamBaseOffset is the size of the FeatureList and its records..
+         */
+        fixFeatureParamOffsets(g, t, (short)size);
+        offset += t->params_size;
+    }
 
     prepLookupList(g, t);
     t->tbl.LookupList = offset;
     t->lookupSize = fillLookupList(g, t);
-
-    if (t->nFeatParams > 0) {
-        /* The feature table FeatureParam offsets are currently from the start of the subtable block.*/
-        /* featureParamBaseOffset is the (size of the feature list + feature record array) + size of the lookup list. */
-        long featureParamBaseOffset = (t->tbl.LookupList - t->tbl.FeatureList) + t->lookupSize;
-        fixFeatureParmOffsets(g, t, (short)featureParamBaseOffset);
-    }
 }
 
 void otlTableFillStub(hotCtx g, otlTbl t) {
@@ -1893,6 +1898,13 @@ void otlTableWrite(hotCtx g, otlTbl t) {
             OUT2(feature->LookupListIndex[j]);
         }
     }
+}
+
+void otlLookupListWrite(hotCtx g, otlTbl t) {
+    int i;
+    int j;
+    otlCtx h = g->ctx.otl;
+    Header *hdr = &t->tbl;
 
     /* Write lookup list */
     OUT2(hdr->LookupList_.LookupCount);
@@ -2014,24 +2026,29 @@ void otlSubtableAdd(hotCtx g, otlTbl t, Tag script, Tag language, Tag feature,
     sub->label = label;
     sub->fmt = fmt;
     sub->isFeatParam = isFeatParam;
+
     if (feature == (Tag)TAG_STAND_ALONE) {
         sub->seenInFeature = 0;
     } else {
         sub->seenInFeature = 1;
     }
 
-    if (script == TAG_UNDEF) {
-        t->nAnonSubtables++;
-    }
-    if (feature == (Tag)TAG_STAND_ALONE) {
-        t->nStandAloneSubtables++;
-    }
-
-    /* FeatParam subtables may be labeled, but should NOT be added */
-    /* to the list of real look ups.                               */
-    if (IS_REF_LAB(label)) {
-        t->nRefLookups++;
-    } else if (isFeatParam) {
+    if (isFeatParam) {
         t->nFeatParams++;
+
+        if (IS_REF_LAB(label)) {
+          t->nRefLookups++;
+        }
+    } else {
+        if (script == TAG_UNDEF) {
+          t->nAnonSubtables++;
+        }
+        if (feature == (Tag)TAG_STAND_ALONE) {
+          t->nStandAloneSubtables++;
+        }
+
+        if (IS_REF_LAB(label)) {
+          t->nRefLookups++;
+        }
     }
 }

c/makeotf/makeotf_lib/source/hotconv/GPOS.c
@@ -152,6 +152,8 @@ typedef struct { /* New subtable data */
 struct GPOSCtx_ {
     SubtableInfo new;
     struct {
+        LOffset featParam;         /* (Cumulative.) Next subtable offset |->  */
+
         LOffset subtable;         /* (Cumulative.) Next subtable offset |->  */
                                   /* start of subtable section. LOffset to   */
                                   /* check for overflow                      */
@@ -332,7 +334,8 @@ void GPOSNew(hotCtx g) {
 #if HOT_DEBUG
     h->new.single.func = initSingle;
 #endif
-    h->offset.subtable = h->offset.extension = h->offset.extensionSection = 0;
+    h->offset.subtable = h->offset.featParam = 0;
+    h->offset.extension = h->offset.extensionSection = 0;
     dnaINIT(g->dnaCtx, h->values, 1000, 500);
     dnaINIT(g->dnaCtx, h->subtables, 10, 10);
     dnaINIT(g->dnaCtx, h->anonSubtable, 3, 10);
@@ -373,6 +376,8 @@ int GPOSFill(hotCtx g) {
 #endif /* HOT_FEAT_SUPPORT */
 
     /* Add OTL features */
+    /* See GSUB.c::GSUBFill() for an explanation of the subtable order */
+
     for (i = 0; i < h->subtables.cnt; i++) {
         Subtable *sub = &h->subtables.array[i];
         int isExt = sub->extension.use;
@@ -388,7 +393,7 @@ int GPOSFill(hotCtx g) {
     }
     DF(1, (stderr, "### GPOS:\n"));
 
-    otlTableFill(g, h->otl);
+    otlTableFill(g, h->otl, h->offset.featParam);
 
     h->offset.extensionSection = h->offset.subtable + otlGetCoverageSize(h->otl) + otlGetClassSize(h->otl);
 
@@ -411,12 +416,35 @@ int GPOSFill(hotCtx g) {
     return 1;
 }
 
+static void featParamsWrite(hotCtx g, GPOSCtx h) {
+    int i;
+
+    for (i = 0; i < h->subtables.cnt; i++) {
+        Subtable *sub = &h->subtables.array[i];
+        if (IS_REF_LAB(sub->label)) {
+            continue;
+        }
+
+        switch (sub->lkpType) {
+                /* I am keep the case switch from GPOS Write in case
+                 we ever have  new GPOS feature param. */
+                case GPOSFeatureParam:
+                    writeFeatParam(h, sub);
+                    break;
+        }
+    }
+}
+
+
 void GPOSWrite(hotCtx g) {
     int i;
     GPOSCtx h = g->ctx.GPOS;
 
     /* Write OTL features */
     otlTableWrite(g, h->otl);
+    featParamsWrite(g, h);
+    otlLookupListWrite(g, h->otl);
+
 
     /* Write main subtable section */
     for (i = 0; i < h->subtables.cnt; i++) {
@@ -441,10 +469,6 @@ void GPOSWrite(hotCtx g) {
                 writePairPos(g, h, sub);
                 break;
 
-            case GPOSFeatureParam:
-                writeFeatParam(h, sub);
-                break;
-
             case GPOSChain:
                 writeChainPos(g, h, sub);
                 break;
@@ -467,6 +491,8 @@ void GPOSWrite(hotCtx g) {
 
             case GPOSContext:
                 break;
+            /* We do not write the feature param in this loop. That
+             happens in featParamsWrite(). */
         }
     }
 
@@ -524,7 +550,6 @@ void GPOSReuse(hotCtx g) {
 
     h->new.script = h->new.language = h->new.feature = TAG_UNDEF;
 
-    /* Free subtables */
     for (i = 0; i < h->subtables.cnt; i++) {
         Subtable *sub = &h->subtables.array[i];
 
@@ -578,7 +603,9 @@ void GPOSReuse(hotCtx g) {
     h->new.baseList.cnt = 0;
     h->new.single.cnt = 0;
     h->new.pairs.cnt = 0;
-    h->offset.subtable = h->offset.extension = h->offset.extensionSection = 0;
+    h->offset.featParam = h->offset.subtable = 0;
+    h->offset.extension = h->offset.extensionSection = 0;
+
     h->values.cnt = 0;
     h->subtables.cnt = 0;
 
@@ -665,7 +692,15 @@ static void reuseClassDefs(GPOSCtx h) {
 
 static void startNewSubtable(hotCtx g) {
     GPOSCtx h = g->ctx.GPOS;
-    Subtable *sub = h->new.sub = dnaNEXT(h->subtables);
+    Subtable *sub;
+    int hasFeatureParam = h->new.lkpType == GPOSFeatureParam;
+
+    sub = h->new.sub = dnaNEXT(h->subtables);
+    if (hasFeatureParam) {
+        sub->offset = h->offset.featParam;
+    } else {
+        sub->offset = h->offset.subtable;
+    }
 
     sub->script = h->new.script;
     sub->language = h->new.language;
@@ -675,10 +710,9 @@ static void startNewSubtable(hotCtx g) {
     sub->lkpFlag = h->new.lkpFlag;
     sub->markSetIndex = h->new.markSetIndex;
     sub->label = h->new.label;
-    sub->offset = h->offset.subtable;
 
     sub->extension.use = h->new.useExtension;
-    if (h->new.useExtension && !IS_REF_LAB(h->new.label)) {
+    if (h->new.useExtension && (!IS_REF_LAB(h->new.label)) && (!hasFeatureParam)) {
         sub->extension.otl = otlTableNew(g);
         sub->extension.offset = h->offset.extension; /* Not needed */
         sub->extension.tbl = fillExtensionPos(g, h, h->new.lkpType);
@@ -1027,7 +1061,7 @@ void GPOSAddSize(hotCtx g, short *params, unsigned short numParams) {
     }
 
     sub->tbl = feat_param;
-    h->offset.subtable += uint16 * 5; /* When we write it out, we leave off the initial numParams short value.*/
+    h->offset.featParam += uint16 * 5; /* When we write it out, we leave off the initial numParams short value.*/
 }
 
 void GPOSSetSizeMenuNameID(hotCtx g, unsigned short nameID) {

c/makeotf/makeotf_lib/source/hotconv/GSUB.c
@@ -98,6 +98,7 @@ typedef struct {
 struct GSUBCtx_ {
     SubtableInfo new;
     struct {
+        LOffset featParam;        /* (Cumulative.) Next subtable offset      */
         LOffset subtable;         /* (Cumulative.) Next subtable offset |->  */
                                   /* start of subtable section. LOffset to   */
                                   /* check for overflow                      */
@@ -196,7 +197,8 @@ void GSUBNew(hotCtx g) {
     h->new.script = h->new.language = h->new.feature = TAG_UNDEF;
 
     dnaINIT(g->dnaCtx, h->new.rules, 50, 200);
-    h->offset.subtable = h->offset.extension = h->offset.extensionSection = 0;
+    h->offset.featParam = h->offset.subtable = 0;
+    h->offset.extension = h->offset.extensionSection = 0;
     dnaINIT(g->dnaCtx, h->subtables, 10, 10);
     dnaINIT(g->dnaCtx, h->anonSubtable, 3, 10);
     h->anonSubtable.func = subtableInit;
@@ -230,6 +232,34 @@ int GSUBFill(hotCtx g) {
 #endif /* HOT_FEAT_SUPPORT */
 
     /* Add OTL features */
+
+    /* The font tables are in the order:
+     ScriptList
+     FeastureList
+     FeatureParams
+     LookupList
+     lookup subtables
+     anon subtables (lookup subtables created by contextual rules)
+     coverage defintion tables
+     class definition tables
+     extension sections.
+     Notes:
+     All lookup/coverage/class tables are added in the order that they
+     are created by the feature file. All offsets are set to the current
+     end of the subtable list when they are created. The only exceptions
+     are the sub-tables for the aalt lookups. These are created after
+     the end of fearure file parsing, in feat.c:featFill(), since the
+     aalt feature references can be used only after all the other
+     features are defined.
+     */
+
+    /* For featparams and lookup subtables, there are two parallel sets of arrays
+    of subtables. The GSUB arrays (h->*) contain the actual data to be written,
+    and is where the offsets are set. The others are in the otl table, and exists
+    so that the GPOS and GSUB can share code for ordering and writing
+    feature and lookup indices. The latter inherit offset and other data
+    from the GSUB arrays. The otl table arrays are created below. The GSUB arrays
+    are created when the feature file is processed, by all the fill* functions. */
     for (i = 0; i < h->subtables.cnt; i++) {
         Subtable *sub = &h->subtables.array[i];
         int isExt = sub->extension.use;
@@ -248,7 +278,7 @@ int GSUBFill(hotCtx g) {
     }
     DF(1, (stderr, "### GSUB:\n"));
 
-    otlTableFill(g, h->otl);
+    otlTableFill(g, h->otl, h->offset.featParam);
 
     h->offset.extensionSection = h->offset.subtable + otlGetCoverageSize(h->otl) + otlGetClassSize(h->otl);
 #if HOT_DEBUG
@@ -270,6 +300,27 @@ int GSUBFill(hotCtx g) {
     return 1;
 }
 
+static void featParamsWrite(hotCtx g, GSUBCtx h) {
+    int i;
+
+    for (i = 0; i < h->subtables.cnt; i++) {
+        Subtable *sub = &h->subtables.array[i];
+        if (IS_REF_LAB(sub->label)) {
+            continue;
+        }
+
+        switch (sub->lkpType) {
+                case GSUBFeatureNameParam:
+                    writeGSUBFeatNameParam(h, sub);
+                    break;
+
+                case GSUBCVParam:
+                    writeGSUBCVParam(h, sub);
+                    break;
+        }
+    }
+}
+
 void GSUBWrite(hotCtx g) {
     int i;
     GSUBCtx h = g->ctx.GSUB;
@@ -280,10 +331,14 @@ void GSUBWrite(hotCtx g) {
         h->otl = otlTableNew(g);
         otlTableFillStub(g, h->otl);
         otlTableWrite(g, h->otl);
+        /* don't need to call featParamsWrite(): no features without lookup subtables. */
+        otlLookupListWrite(g, h->otl);
         return;
     }
 
-    otlTableWrite(g, h->otl);
+    otlTableWrite(g, h->otl); /* write header, ScriptList and FeatureList.*/
+    featParamsWrite(g, h); /* write the feature param subtables after the FeatureList */
+    otlLookupListWrite(g, h->otl);
 
     /* Write main subtable section */
     for (i = 0; i < h->subtables.cnt; i++) {
@@ -325,13 +380,8 @@ void GSUBWrite(hotCtx g) {
                 writeReverseChain(g, h, sub);
                 break;
 
-            case GSUBFeatureNameParam:
-                writeGSUBFeatNameParam(h, sub);
-                break;
-
-            case GSUBCVParam:
-                writeGSUBCVParam(h, sub);
-                break;
+            /* We no longer write the feature param subtables as part of the LookupList
+             they are instead written above in featParamsWrite().*/
 
             /* Not supported
             case GSUBContext:
@@ -449,7 +499,8 @@ void GSUBReuse(hotCtx g) {
     }
 
     h->new.rules.cnt = 0;
-    h->offset.subtable = h->offset.extension = h->offset.extensionSection = 0;
+    h->offset.featParam = h->offset.subtable = 0;
+    h->offset.extension = h->offset.extensionSection = 0;
     h->subtables.cnt = 0;
 
     h->anonSubtable.cnt = 0;
@@ -506,6 +557,8 @@ void GSUBFeatureBegin(hotCtx g, Tag script, Tag language, Tag feature) {
 static void startNewSubtable(hotCtx g) {
     GSUBCtx h = g->ctx.GSUB;
     Subtable *sub = h->new.sub = dnaNEXT(h->subtables);
+    int hasFeatureParam = ((h->new.lkpType == GSUBFeatureNameParam) ||
+                           (h->new.lkpType == GSUBCVParam));
 
     sub->script = h->new.script;
     sub->language = h->new.language;
@@ -515,10 +568,14 @@ static void startNewSubtable(hotCtx g) {
     sub->lkpFlag = h->new.lkpFlag;
     sub->markSetIndex = h->new.markSetIndex;
     sub->label = h->new.label;
-    sub->offset = h->offset.subtable;
+    if (hasFeatureParam) {
+        sub->offset = h->offset.featParam;
+    } else {
+        sub->offset = h->offset.subtable;
+    }
 
     sub->extension.use = h->new.useExtension;
-    if (h->new.useExtension && !IS_REF_LAB(h->new.label)) {
+    if (h->new.useExtension && (!IS_REF_LAB(h->new.label)) && (!hasFeatureParam)) {
         sub->extension.otl = otlTableNew(g);
         sub->extension.offset = h->offset.extension; /* Not needed */
         sub->extension.tbl = fillExtension(g, h, h->new.lkpType);
@@ -809,7 +866,7 @@ void GSUBAddFeatureMenuParam(hotCtx g, void *param) {
     feat_param->version = 0;
     feat_param->nameID = *((unsigned short *)param);
     sub->tbl = feat_param;
-    h->offset.subtable += param_size;
+    h->offset.featParam += param_size;
 }
 
 static void fillGSUBCVParam(hotCtx g, GSUBCtx h, Subtable *sub) {
@@ -898,7 +955,7 @@ void GSUBAddCVParam(hotCtx g, void *param) {
     }
 
     sub->tbl = new_param;
-    h->offset.subtable += param_offset;
+    h->offset.featParam += param_offset;
 }
 
 /* -------------------------- Single Substitution -------------------------- */

c/makeotf/makeotf_lib/source/hotconv/otl.h
@@ -16,9 +16,10 @@ void otlFree(hotCtx g);
 typedef struct otlTbl_ *otlTbl;
 
 otlTbl otlTableNew(hotCtx g);
-void otlTableFill(hotCtx g, otlTbl t);
+void otlTableFill(hotCtx g, otlTbl t, LOffset params_size);
 void otlTableFillStub(hotCtx g, otlTbl t);
 void otlTableWrite(hotCtx g, otlTbl t);
+void otlLookupListWrite(hotCtx g, otlTbl t);
 void otlTableReuse(hotCtx g, otlTbl t);
 void otlTableFree(hotCtx g, otlTbl t);
 

tests/makeotfexe_test.py
@@ -427,6 +427,8 @@ def test_overflow_bug731():
 
 
 def test_parameter_offset_overflow_bug746():
+    # Since the fix for issue 1017, the test is that the feature param offset
+    # does NOT overflow.
     input_filename = 'bug746/font.pfa'
     feat_filename = 'bug746/feat.fea'
     otf_path = get_temp_file_path()
@@ -439,7 +441,7 @@ def test_parameter_offset_overflow_bug746():
 
     with open(stderr_path, 'rb') as f:
         output = f.read()
-    assert(b"[FATAL] <bug746> feature parameter offset too large") in output
+    assert(b"[FATAL] <bug746> FeatParam offset too large") not in output
 
 
 def test_base_anchor_bug811():

commit 21214e66e1dd794cb252ef46238b646daa7c7ada

[makeotfexe] Fix issue 1080: Bad font built when feature file has multiple languagesystems and feature parameters #1080
Updated test file to fail the build tests with in versions of makeotf with this bug, and to pass in versions without this bug, by changing from one to three language system statements, and updating the expected ttx file.

This bug  was introduced in commit https://github.com/adobe-type-tools/afdko/commit/5624e02b58390e605639c3633025cd73cd418da7, when fixing how feature parameter tables were ordered in the GSUB/GPOS tables. The bug in in otl.c after line 2035, where bug was in incrementing nFeatureParams for every feature param subtable, instead of skipping the ones that were just references. Fixed by reverting to the original code block for this. The wrong code was left over from my first pass at changing with the subtable organization, and I forgot to revert this code when I pulled that out and used a different approach.

c/makeotf/makeotf_lib/source/hotconv/otl.c
@@ -2033,22 +2033,18 @@ void otlSubtableAdd(hotCtx g, otlTbl t, Tag script, Tag language, Tag feature,
         sub->seenInFeature = 1;
     }
 
-    if (isFeatParam) {
-        t->nFeatParams++;
+    if (script == TAG_UNDEF) {
+         t->nAnonSubtables++;
+     }
+     if (feature == (Tag)TAG_STAND_ALONE) {
+         t->nStandAloneSubtables++;
+     }
 
-        if (IS_REF_LAB(label)) {
-          t->nRefLookups++;
-        }
-    } else {
-        if (script == TAG_UNDEF) {
-          t->nAnonSubtables++;
-        }
-        if (feature == (Tag)TAG_STAND_ALONE) {
-          t->nStandAloneSubtables++;
-        }
-
-        if (IS_REF_LAB(label)) {
-          t->nRefLookups++;
-        }
-    }
+     /* FeatParam subtables may be labeled, but should NOT be added */
+     /* to the list of real look ups.                               */
+     if (IS_REF_LAB(label)) {
+         t->nRefLookups++;
+     } else if (isFeatParam) {
+         t->nFeatParams++;
+     }
 }

tests/makeotfexe_data/input/bug1017/feat.fea
@@ -1,4 +1,6 @@
 languagesystem DFLT dflt;
+languagesystem latn dflt;
+languagesystem grek dflt;
 
 
 feature ss05 {