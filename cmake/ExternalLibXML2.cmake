db57afaa926bb4111d51034f323b6dae2c4f9f39
Kamile Demir <demir@adobe.com>
2022-04-12 14:56:39 -0700

    [libxml2] Building libxml2 from source (working on mac)

--- cmake/ExternalLibXML2.cmake
+++ cmake/ExternalLibXML2.cmake
@@ -1,152 +1,123 @@
-include(ExternalProject)
-cmake_policy(SET CMP0114 NEW)  # Added by iterumllc
+# set(libxml2_release "2.9")
+# set(libxml2_patch_version 0)
+# set(libxml2_url "ftp://xmlsoft.org/libxml2/libxml2-sources-${libxml2_release}.${libxml2_patch_version}.tar.gz")
+# set(libxml2_md5 "7da7af8f62e111497d5a2b61d01bd811")
 
-# Edits below are for path length or (in the zip download) correctness
+# #We need to state that we're dependent on ZLib so build order is correct
+# set(_XML2_DEPENDS ZLib)
 
-set(LIBXML2_ROOT ${CMAKE_CURRENT_BINARY_DIR}/l2/src/l2)  # Edited by iterumllc
-set(LIBXML2_INCLUDE_DIRS ${LIBXML2_ROOT}/include/libxml2)
+#This build requires make, ensure we have it, or error out.
+# if(CMAKE_GENERATOR MATCHES ".*Makefiles")
+#   set(MAKE_EXECUTABLE "$(MAKE)")
+# else()
+#   find_program(MAKE_EXECUTABLE make)
+#   if(NOT MAKE_EXECUTABLE)
+#     message(FATAL_ERROR "Could not find 'make', required to build libxml2.")
+#   endif()
+# endif()
+
+# include (ExternalProject)
+# ExternalProject_Add(libxml2
+#   DEPENDS ${_XML2_DEPENDS}
+#   URL ${libxml2_url}
+#   URL_MD5 ${libxml2_md5}
+#   PREFIX  ${test_ext_proj_BUILD_PREFIX}
+#   DOWNLOAD_DIR ${test_ext_proj_DOWNLOAD_DIR}
+#   INSTALL_DIR  ${test_ext_proj_BUILD_INSTALL_PREFIX}
+#   BUILD_IN_SOURCE 1
+#   CONFIGURE_COMMAND ./configure
+#     --prefix=${test_ext_proj_BUILD_INSTALL_PREFIX}
+#     --with-zlib=${ZLIB_ROOT}
+#   BUILD_COMMAND ${MAKE_EXECUTABLE}
+#   INSTALL_COMMAND ${MAKE_EXECUTABLE} install
+# )
+
+set(LIBXML2_TARGET external.libxml2)
+set(LIBXML2_INSTALL_DIR ${CMAKE_CURRENT_BINARY_DIR}/${LIBXML2_TARGET})
+set(LIBXML2_SRC_DIR ${LIBXML2_INSTALL_DIR}/src/${LIBXML2_TARGET})
+set(LIBXML2_INCLUDE_DIRS ${LIBXML2_INSTALL_DIR}/include/libxml2)
+set(LIBXML2_LIBRARY
+        ${LIBXML2_INSTALL_DIR}/lib/libxml2.a)
 include_directories(${LIBXML2_INCLUDE_DIRS})
+# set(LIBXML2_BINARY_DIR ${LIBXML2_INSTALL_DIR}/kamile_build)
+
+message("LIBXML2_INSTALL_DIR: ${LIBXML2_INSTALL_DIR}")
+message("LIBXML2_SRC_DIR: ${LIBXML2_SRC_DIR}")
+message("LIBXML2_INCLUDE_DIRS: ${LIBXML2_INCLUDE_DIRS}")
+
 set(LIBXML2_GIT_REPOSITORY https://gitlab.gnome.org/GNOME/libxml2.git)
 if(NOT DEFINED LIBXML2_TAG)
-  # Set to branch name to keep library updated at the cost of needing to rebuild after 'clean'
-  # Set to commit hash to keep the build stable and does not need to rebuild after 'clean'
   set(LIBXML2_TAG master)
 endif()
 
-if(${CMAKE_GENERATOR} MATCHES "Visual Studio.*")
-  set(LIBXML2_OUTPUT_DIR ${LIBXML2_ROOT}/$(Configuration))
-elseif(${CMAKE_GENERATOR} MATCHES "Xcode.*")
-  set(LIBXML2_OUTPUT_DIR ${LIBXML2_ROOT}/$(CONFIGURATION))
-else()
-  set(LIBXML2_OUTPUT_DIR ${LIBXML2_ROOT})
-endif()
+# list(APPEND LIBXML2_LIBRARIES xml2)
+# foreach(lib IN LISTS LIBXML2_LIBRARIES)
+#   list(APPEND LIBXML2_BUILD_BYPRODUCTS ${LIBXML2_INSTALL_DIR}/lib/lib${lib}.a)
 
+#   add_library(${lib} STATIC IMPORTED)
+#   set_property(TARGET ${lib} PROPERTY IMPORTED_LOCATION
+#                ${LIBXML2_INSTALL_DIR}/lib/lib${lib}.a)
+#   add_dependencies(${lib} ${LIBXML2_TARGET})
+#   message("Byproduct library: ${lib}")
+#   message(${lib})
+# endforeach(lib)
+# message("LIBXML2_BUILD_BYPRODUCTS: ${LIBXML2_BUILD_BYPRODUCTS}")
 
-if(MSVC)
-  set(LIBXML2_STATIC_LIBRARIES
-      ${LIBXML2_OUTPUT_DIR}/LIBXML2-runtime-static.lib)
-  set(LIBXML2_SHARED_LIBRARIES
-      ${LIBXML2_OUTPUT_DIR}/LIBXML2-runtime.lib)
-  set(LIBXML2_RUNTIME_LIBRARIES
-      ${LIBXML2_OUTPUT_DIR}/LIBXML2-runtime.dll)
-else()
-  set(LIBXML2_STATIC_LIBRARIES
-      ${LIBXML2_OUTPUT_DIR}/libLIBXML2-runtime.a)
-  if(MINGW)
-    set(LIBXML2_SHARED_LIBRARIES
-        ${LIBXML2_OUTPUT_DIR}/libLIBXML2-runtime.dll.a)
-    set(LIBXML2_RUNTIME_LIBRARIES
-        ${LIBXML2_OUTPUT_DIR}/libLIBXML2-runtime.dll)
-  elseif(CYGWIN)
-    set(LIBXML2_SHARED_LIBRARIES
-        ${LIBXML2_OUTPUT_DIR}/libLIBXML2-runtime.dll.a)
-    set(LIBXML2_RUNTIME_LIBRARIES
-        ${LIBXML2_OUTPUT_DIR}/cygLIBXML2-runtime-4.9.2.dll)
-  elseif(APPLE)
-    set(LIBXML2_RUNTIME_LIBRARIES
-        ${LIBXML2_OUTPUT_DIR}/libLIBXML2-runtime.dylib)
-  else()
-    set(LIBXML2_RUNTIME_LIBRARIES
-        ${LIBXML2_OUTPUT_DIR}/libLIBXML2-runtime.so)
-  endif()
-endif()
+message("\nSOURCE DIR: ${LIBXML2_SRC_DIR}/autogen.sh\n")
+include (ExternalProject)
+ExternalProject_Add(${LIBXML2_TARGET}
+  PREFIX ${LIBXML2_TARGET}
+  GIT_REPOSITORY ${LIBXML2_GIT_REPOSITORY}
+  GIT_TAG ${LIBXML2_TAG}
+  SOURCE_DIR ${LIBXML2_SRC_DIR}
+  CONFIGURE_COMMAND ${LIBXML2_SRC_DIR}/autogen.sh --without-python
+                                                  --prefix=${LIBXML2_INSTALL_DIR}
+  BYPRODUCTS ${LIBXML2_LIBRARY}
+  BUILD_COMMAND make
+  INSTALL_COMMAND make install
+  # WORKING_DIRECTORY ${LIBXML2_SRC_DIR}
+  # BUILD_IN_SOURCE 1
+)
 
+ExternalProject_Get_Property(${LIBXML2_TARGET} SOURCE_DIR)
+# ExternalProject_Get_Property(sphinxbase BINARY_DIR)
+# SET(SPHINXBASE_SOURCE_DIR ${SOURCE_DIR})
+# SET(SPHINXBASE_BINARY_DIR ${BINARY_DIR})
+# SET(SPHINXBASE_LIBRARIES ${SPHINXBASE_BINARY_DIR}/src/libsphinxbase/.libs/libsphinxbase.a)
+# INCLUDE_DIRECTORIES(${SPHINXBASE_SOURCE_DIR}/include)
 
-if(${CMAKE_GENERATOR} MATCHES ".* Makefiles")
-  # This avoids
-  # 'warning: jobserver unavailable: using -j1. Add '+' to parent make rule.'
-  set(LIBXML2_BUILD_COMMAND $(MAKE))
-elseif(${CMAKE_GENERATOR} MATCHES "Visual Studio.*")
-  set(LIBXML2_BUILD_COMMAND
-      ${CMAKE_COMMAND}
-          --build .
-          --config $(Configuration)
-          --target)
-elseif(${CMAKE_GENERATOR} MATCHES "Xcode.*")
-  set(LIBXML2_BUILD_COMMAND
-      ${CMAKE_COMMAND}
-          --build .
-          --config $(CONFIGURATION)
-          --target)
-else()
-  set(LIBXML2_BUILD_COMMAND
-      ${CMAKE_COMMAND}
-          --build .
-          --target)
-endif()
+add_library(libxml2 STATIC IMPORTED)
+set_property(TARGET libxml2 PROPERTY IMPORTED_LOCATION ${LIBXML2_LIBRARY}
+            )
+add_dependencies(libxml2 ${LIBXML2_TARGET})
 
+# include_directories(${LIBXML2_INSTALL_DIR}/include)
 
-# if(NOT DEFINED LIBXML2_WITH_STATIC_CRT)
-#   set(LIBXML2_WITH_STATIC_CRT ON)
-# endif()
+# add_dependencies(mergefonts ${LIBXML2_LIBRARY})
 
+# ExternalProject_Add_Step(${LIBXML2_TARGET}
+#   PREFIX ${LIBXML2_TARGET}
+#   CONFIGURE_COMMAND ${LIBXML2_SRC_DIR}/autogen.sh --without-python --prefix=${LIBXML2_INSTALL_DIR}
+#   BUILD_COMMAND make
+#   WORKING_DIRECTORY ${LIBXML2_SRC_DIR}
+# )
 
-ExternalProject_Add(
-    libxml2_runtime
-    PREFIX l2  # Edited by iterumllc
-    GIT_REPOSITORY ${LIBXML2_GIT_REPOSITORY}
-    GIT_TAG ${LIBXML2_TAG}
-    DOWNLOAD_DIR ${CMAKE_CURRENT_BINARY_DIR}
-    BUILD_COMMAND ""
-    BUILD_IN_SOURCE 1
-    SOURCE_DIR ${LIBXML2_ROOT}
-    SOURCE_SUBDIR macos/src
-    # CMAKE_CACHE_ARGS
-    #     -DCMAKE_BUILD_TYPE:STRING=${CMAKE_BUILD_TYPE}
-    #     -DWITH_STATIC_CRT:BOOL=${LIBXML2_WITH_STATIC_CRT}
-        # -DCMAKE_CXX_STANDARD:STRING=17 # if desired, compile the runtime with a different C++ standard
-        # -DCMAKE_CXX_STANDARD:STRING=${CMAKE_CXX_STANDARD} # alternatively, compile the runtime with the same C++ standard as the outer project
-    INSTALL_COMMAND ""
-    EXCLUDE_FROM_ALL 1)
+# ExternalProject_Add_Step(
+#   ${LIBXML2_TARGET}
+#   CONFIGURE_COMMAND <SOURCE_DIR>/autogen.sh --without-python
+# )
 
+# ExternalProject_Get_Property(${LIBXML2_TARGET} BINARY_DIR)
+# file(MAKE_DIRECTORY ${LIBXML2_SRC_DIR})
+# set(libxml2_static ${LIBXML2_SRC_DIR}/libOpenCL.so
+#   CACHE PATH "libxml2 statuc library" FORCE
+# )
 
-# Separate build step as rarely people want both
-set(LIBXML2_BUILD_DIR ${LIBXML2_ROOT})
-if(${CMAKE_VERSION} VERSION_GREATER_EQUAL "3.14.0")
-  # CMake 3.14 builds in above's SOURCE_SUBDIR when BUILD_IN_SOURCE is true
-  set(LIBXML2_BUILD_DIR ${LIBXML2_ROOT})
-endif()
+# add_library(libxml_static SHARED IMPORTED)
+# add_dependencies(libxml_static ${LIBXML2_TARGET})
+# set_target_properties(libxml_static
+#     PROPERTIES IMPORTED_LOCATION ${LIBXML2_BUILD_BYPRODUCTS}
+# )
 
-message("----LINE 135---\n")
-
-ExternalProject_Add_Step(
-    libxml2_runtime
-    build_static
-    COMMAND ${LIBXML2_BUILD_COMMAND} libxml2_static
-    # Depend on target instead of step (a custom command)
-    # to avoid running dependent steps concurrently
-    DEPENDS libxml2_runtime
-    BYPRODUCTS ${LIBXML2_STATIC_LIBRARIES}
-    EXCLUDE_FROM_MAIN 1
-    WORKING_DIRECTORY ${LIBXML2_BUILD_DIR})
-ExternalProject_Add_StepTargets(libxml2_runtime)
-
-message("----LINE 149---\n")
-
-add_library(libxml2_static STATIC IMPORTED)
-add_dependencies(libxml2_static libxml2_runtime-build_static)
-set_target_properties(libxml2_static PROPERTIES
-                      IMPORTED_LOCATION ${LIBXML2_STATIC_LIBRARIES})
-
-ExternalProject_Add_Step(
-    libxml2_runtime
-    build_shared
-    COMMAND ${LIBXML2_BUILD_COMMAND} libxml2_shared
-    # Depend on target instead of step (a custom command)
-    # to avoid running dependent steps concurrently
-    DEPENDS libxml2_runtime
-    BYPRODUCTS ${LIBXML2_SHARED_LIBRARIES} ${LIBXML2_RUNTIME_LIBRARIES}
-    EXCLUDE_FROM_MAIN 1
-    WORKING_DIRECTORY ${LIBXML2_BUILD_DIR})
-ExternalProject_Add_StepTargets(libxml2_runtime)
-
-add_library(libxml2_shared SHARED IMPORTED)
-add_dependencies(libxml2_shared libxml2_runtime-build_shared)
-set_target_properties(libxml2_shared PROPERTIES
-                      IMPORTED_LOCATION "${LIBXML2_RUNTIME_LIBRARIES}")
-if(LIBXML2_SHARED_LIBRARIES)
-  set_target_properties(libxml2_shared PROPERTIES
-                        IMPORTED_IMPLIB "${LIBXML2_SHARED_LIBRARIES}")
-endif()
-
-message("----SUCCESS!!---\n")
+# target_link_libraries()
+# add_library(${LIBXML2_TARGET})
