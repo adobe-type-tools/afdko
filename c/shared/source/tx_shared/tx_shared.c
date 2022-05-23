07a78996ef13c5ea83af1bad9c389cc185124f7d
Kamile Demir <demir@adobe.com>
2022-05-23 15:50:12 -0700

    cpplint fix

--- c/shared/source/tx_shared/tx_shared.c
+++ c/shared/source/tx_shared/tx_shared.c
@@ -411,7 +411,6 @@ static size_t stm_xml_read(ctlStreamCallbacks *cb, void *stream, char **ptr, xml
 
     Stream *s = stream;
     txCtx h = cb->direct_ctx;
-    
     res = fread(s->buf, 1, 4, s->fp);
     if (res > 0) {
         ctxt = xmlCreatePushParserCtxt(NULL, NULL,
