741b5fcc343571a4b95f99e9c23ed3faa6861219
第七詝 <zhou@0h7z.com>
2022-07-04 07:46:39 +0000

    Update otf2ttf.py

--- python/afdko/otf2ttf.py
+++ python/afdko/otf2ttf.py
@@ -152,14 +152,15 @@ def main(args=None):
 
     files = list(chain.from_iterable(map(glob.glob, options.input)))
 
-    # Set the pool capacity to be the minimum of file quantity and CPU count
-    maxPoolCapacity = min(os.cpu_count(), len(files))
+    # Set the pool capacity to be the minimum of file quantity and CPU count,
+    # at least 1.
+    poolCapacity = max(min(os.cpu_count(), len(files)), 1)
     # Limit parallel capacity to 60 on win32 to avoid WaitForMultipleObjects
     # errors. See https://bugs.python.org/issue45077
-    if sys.platform == "win32" and maxPoolCapacity >= 60:
-        maxPoolCapacity = 60
+    if poolCapacity > 60 and sys.platform == "win32":
+        poolCapacity = 60
     # Do not use "with" statement, or code coverage will malfunction.
-    pool = Pool(maxPoolCapacity)
+    pool = Pool(poolCapacity)
     try:
         pool.map(partial(run, options=options), files)
     finally:
