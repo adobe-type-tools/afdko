4da854810dfa408e39b2b7223b8b1f990e8cd1a6
Kamile Demir <demir@adobe.com>
2023-05-05 15:06:56 -0700

    [tx_tests] move the formerly new tests to the glif parsing tests

--- tests/tx_test.py
+++ tests/tx_test.py
@@ -1505,8 +1505,8 @@ def test_ufo_contentsplist_parsing(file, msg, ret_code):
     ("empty-flexlist", b'', 0),
     ("empty-stems", b'', 0),
     ("empty-stems-2", b'', 0),
-    ("missing-glif", b'Failed to open glif file in parseGLIF:' +
-                     b' glyphs/missing.glif.', 3),
+    ("missing-glif", b'tx: (ufr) Failed to open the ' +
+                     b'glyphs/missing.glif glif file.', 3),
     ("one-stem-hstem", b'', 0),
     ("one-stem-hstem3", b'', 0),
     ("wrong-amount-stems-hstem", b'', 0),
@@ -1523,8 +1523,6 @@ def test_ufo_contentsplist_parsing(file, msg, ret_code):
     ("missing-autohint", b'', 0),
     ("missing-autohint-2", b'', 0),
     ("missing-contours", b'', 0),
-    ("missing-glif", b"Failed to open glif file in parseGLIF:" +
-                     b" glyphs/missing.glif.", 3),
     ("missing-hintsetlist", b'', 0),
     ("missing-outline", b'', 0),
     ("missing-point-tag", b'', 0),
@@ -1532,7 +1530,19 @@ def test_ufo_contentsplist_parsing(file, msg, ret_code):
     ("missing-width2", b'', 0),
     ("missing-flexlist", b'', 0),
     ("FDArraySelect.17.VKana_subset", b"glyph 'monkey' missing" +
-                                      b" CID number in <lib> dict", 6)
+                                      b" CID number in <lib> dict", 6),
+    ("qcurve-glyph",
+     b"tx: (ufr) Encountered unsupported point type 'qcurve' in " +
+     b"glyph 'oops'.", 6),
+    ("null-filepath-glyph",
+     b"tx: (ufr) Encountered glyph reference 'oops' " +
+     b"in contents.plist with an empty file path.", 6),
+    ("null-filepath-cid",
+     b"tx: (ufr) Encountered glyph reference 'cid00000' in " +
+     b"contents.plist with an empty file path.", 6),
+    ("null-component-filepath",
+     b"tx: (ufr) Encountered glyph component reference " +
+     b"'glyphs/C_acute.glif' with an empty file path.", 1)
 ])
 def test_ufo_glifs_parsing(file, msg, ret_code):
     folder = "ufo-glifs-parsing/"
@@ -1668,31 +1678,3 @@ def test_alt_missing_glyph():
     expected_path = generate_ps_dump(expected_path)
     output_path = generate_ps_dump(output_path)
     assert differ([expected_path, output_path])
-
-
-@pytest.mark.parametrize('file, msg', [
-    ("qcurve-glyph.ufo",
-     b"tx: (ufr) Encountered unsupported point type 'qcurve' in " +
-     b"glyph 'oops'."),
-    ("null-filepath-glyph.ufo",
-     b"tx: (ufr) Encountered glyph reference 'oops' " +
-     b"in contents.plist with an empty file path."),
-    ("null-filepath-cid.ufo", 
-     b"tx: (ufr) Encountered glyph reference 'cid00000' in " +
-     b"contents.plist with an empty file path."),
-    ("null-component-filepath.ufo",
-     b"tx: (ufr) Encountered glyph component reference " +
-     b"'glyphs/C_acute.glif' with an empty file path.")
-])
-def test_glyph_error_crash(file, msg):
-    """
-    Tests some cases that were crashing tx rather than erroring due
-    to the use of getBufferContextPtr(h).
-    """
-    input_path = get_input_path(file)
-    arg = CMD + ['-s', '-e', '-a', 't1', '-f',
-                 input_path]
-    stderr_path = runner(arg)
-    with open(stderr_path, 'rb') as f:
-        output = f.read()
-    assert msg in output
