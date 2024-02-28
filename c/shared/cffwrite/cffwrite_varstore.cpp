0414172965f62df1fb99c64f2da1321e7cd541ef
Skef Iterum <github@skef.org>
2024-02-28 15:54:11 -0800

    Reorganize writing of itemVariationStore

--- c/shared/cffwrite/cffwrite_varstore.cpp
+++ c/shared/cffwrite/cffwrite_varstore.cpp
@@ -14,91 +14,32 @@
 #include "ctutil.h"
 #include "cffwrite_dict.h"
 
-/* Save byte int  in DICT. */
-void cfwDictSaveByteInt(DICT *dict, long i) {
-    char *arg = dnaEXTEND(*dict, 1);
-    arg[1] = (unsigned char)(i);
-}
-
-/* Save short int  in DICT. */
-void cfwDictSaveShortInt(DICT *dict, long i) {
-    char *arg = dnaEXTEND(*dict, 2);
-    arg[0] = (unsigned char)(i >> 8);
-    arg[1] = (unsigned char)i;
-}
-
-/* Save long int  in DICT. */
-void cfwDictSaveLongInt(DICT *dict, long i) {
-    char *arg = dnaEXTEND(*dict, 4);
-    arg[0] = (unsigned char)(i >> 24);
-    arg[1] = (unsigned char)(i >> 16);
-    arg[2] = (unsigned char)(i >> 8);
-    arg[3] = (unsigned char)i;
-}
-
-void writeRegionList(DICT *dst, uint16_t axisCount,
-                     std::vector<itemVariationStore::VariationRegion> &regions) {
-    uint16_t i;
-    cfwDictSaveShortInt(dst, axisCount);
-    cfwDictSaveShortInt(dst, (int16_t) regions.size());
-
-    for (auto &vr : regions) {
-        for (auto &ar: vr) {
-            cfwDictSaveShortInt(dst, std::get<0>(ar));
-            cfwDictSaveShortInt(dst, std::get<1>(ar));
-            cfwDictSaveShortInt(dst, std::get<2>(ar));
-        }
+class cfwVarWriter : public VarWriter {
+ public:
+    cfwVarWriter() = delete;
+    explicit cfwVarWriter(DICT *dict) : dict(dict) {}
+    void w1(char o) override {
+        char *arg = dnaEXTEND(*dict, 1);
+        arg[1] = (unsigned char)o;
     }
-}
-
-void writeDataItemList(DICT *dst,
-                       itemVariationStore::itemVariationDataSubtable &ivd) {
-    uint16_t i, cnt = (uint16_t) ivd.regionIndices.size();
-    cfwDictSaveShortInt(dst, (uint16_t) ivd.deltaValues.size());
-    cfwDictSaveShortInt(dst, 0);
-    cfwDictSaveShortInt(dst, cnt);
-
-    for (i = 0; i < cnt; i++)
-        cfwDictSaveShortInt(dst, ivd.regionIndices[i]);
-}
+    void w2(int16_t o) override {
+        char *arg = dnaEXTEND(*dict, 2);
+        arg[0] = (unsigned char)(o >> 8);
+        arg[1] = (unsigned char)o;
+    }
+    void w4(int32_t o) override {
+        char *arg = dnaEXTEND(*dict, 4);
+        arg[0] = (unsigned char)(o >> 24);
+        arg[1] = (unsigned char)(o >> 16);
+        arg[2] = (unsigned char)(o >> 8);
+        arg[3] = (unsigned char)o;
+    }
+    DICT *dict;
+};
 
 void cfwDictFillVarStore(cfwCtx g, DICT *dst, abfTopDict *top) {
-    int i;
-    itemVariationStore *ivs = top->varStore;
-    unsigned long offset;
+    cfwVarWriter vw {dst};
 
-    // Write place holder for length.
-    cfwDictSaveShortInt(dst, 0);
-
-    // write format
-    cfwDictSaveShortInt(dst, 1);
-
-    // write offset to RegionList.
-    // The Region list immediately follows the IVS header and offset to the dataItems.
-    offset = 8 + ivs->subtables.size() * 4;
-    cfwDictSaveLongInt(dst, offset);
-
-    // write ItemVariationData count
-    cfwDictSaveShortInt(dst, ivs->subtables.size());
-
-    // write the offsets to the ItemVariationData items.
-    offset += ivs->getRegionListSize();
-    for (i = 0; i < ivs->subtables.size(); i++) {
-        cfwDictSaveLongInt(dst, offset);
-        offset += ivs->getDataItemSize(i);
-    }
-
-    writeRegionList(dst, ivs->axisCount, ivs->regions);
-
-    // write the ItemVariationData subtables
-    for (i = 0; i < ivs->subtables.size(); i++) {
-        writeDataItemList(dst, ivs->subtables[i]);
-    }
-
-    // Write length
-    {
-        char *arg = &(dst->array[0]);
-        arg[0] = (unsigned char)(offset >> 8);
-        arg[1] = (unsigned char)offset;
-    }
+    vw.w2((uint16_t)top->varStore->getSize());
+    top->varStore->write(&vw);
 }
