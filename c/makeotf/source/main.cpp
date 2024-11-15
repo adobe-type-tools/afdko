7b1212b9cacd25cb0d603724b357cfe31231953b
Skef Iterum <github@skef.org>
2024-11-15 08:03:35 -0800

    Try to address #1761 -- failures of makeotfexe in the manylinux wheel

--- c/makeotf/source/main.cpp
+++ c/makeotf/source/main.cpp
@@ -1,5 +1,13 @@
 extern "C" int c_main(int argc, char *argv[]);
 
+#ifdef PTHREAD_STATIC_WORKAROUND
+#include <pthread.h>
+#endif
+
 int main(int argc, char *argv[]) {
+#ifdef PTHREAD_STATIC_WORKAROUND
+    static_cast<void>(pthread_create);
+    static_cast<void>(pthread_cancel);
+#endif
     c_main(argc, argv);
 }
