2e35d733825be74b5b48ec6fe23a1ad76adc5dfa
Cristian Le <git@lecris.dev>
2025-06-04 10:46:31 +0200

    Migrate libxml2 ExternalProject to FetchContent

--- cmake/ExternalLibXML2.cmake
+++ cmake/ExternalLibXML2.cmake
@@ -1,51 +1,38 @@
-include (ExternalProject)
-cmake_policy(SET CMP0114 NEW)
+include(FetchContent)
 
-set(LIBXML2_TARGET external.libxml2)
-
-set(LIBXML2_GIT_REPOSITORY https://gitlab.gnome.org/GNOME/libxml2.git)
 if(NOT DEFINED LIBXML2_TAG)
   set(LIBXML2_TAG master)
 endif()
 
-if(${CMAKE_GENERATOR} MATCHES "Visual Studio.*")
-  set(LIBXML2_OUTPUT_DIR ${LIBXML2_TARGET}/bin/${CMAKE_BUILD_TYPE})
-else()
-  set(LIBXML2_OUTPUT_DIR ${LIBXML2_TARGET}/bin)
+set(fetch_content_args)
+if(CMAKE_VERSION VERSION_GREATER_EQUAL 3.24 AND NOT DEFINED ENV{FORCE_BUILD_LIBXML2})
+  list(APPEND fetch_content_args FIND_PACKAGE_ARGS CONFIG)
 endif()
-
-if(WIN32)
-  set(LIBXML2_LIBNAME libxml2s.lib)
-else()
-  set(LIBXML2_LIBNAME libxml2.a)
-endif()
-
-ExternalProject_Add(${LIBXML2_TARGET}
-  GIT_REPOSITORY ${LIBXML2_GIT_REPOSITORY}
-  GIT_TAG ${LIBXML2_TAG}
-  GIT_SHALLOW 1
-  BUILD_BYPRODUCTS ${CMAKE_CURRENT_BINARY_DIR}/${LIBXML2_OUTPUT_DIR}/${LIBXML2_LIBNAME}
-  SOURCE_DIR ${CMAKE_CURRENT_BINARY_DIR}/${LIBXML2_TARGET}/src
-  BINARY_DIR ${CMAKE_CURRENT_BINARY_DIR}/${LIBXML2_TARGET}/bin
-  CMAKE_ARGS -D BUILD_SHARED_LIBS=OFF -D CMAKE_BUILD_TYPE=Release -D LIBXML2_WITH_ICONV=OFF -D LIBXML2_WITH_LZMA=OFF -D LIBXML2_WITH_ZLIB=OFF -D LIBXML2_WITH_PYTHON=OFF -D CMAKE_VERBOSE_MAKEFILE:BOOL=ON
-  INSTALL_COMMAND ""
+FetchContent_Declare(LibXml2
+        GIT_REPOSITORY https://gitlab.gnome.org/GNOME/libxml2.git
+        GIT_TAG ${LIBXML2_TAG}
 )
 
-ExternalProject_Get_Property(${LIBXML2_TARGET} BINARY_DIR)
-ExternalProject_Get_Property(${LIBXML2_TARGET} SOURCE_DIR)
+option(AFDKO_BUILD_LIBXML2_SHARED_LIB
+        "Whether to use the shared library of afdko" OFF)
+if(AFDKO_BUILD_LIBXML2_SHARED_LIB)
+  set(BUILD_SHARED_LIBS ON)
+else()
+  set(BUILD_SHARED_LIBS OFF)
+endif()
+option(LIBXML2_WITH_DEBUG "Override: afdko" OFF)
+option(LIBXML2_WITH_DOCS "Override: afdko" OFF)
+option(LIBXML2_WITH_TESTS "Override: afdko" OFF)
+option(LIBXML2_WITH_PYTHON "Override: afdko" OFF)
+option(LIBXML2_WITH_ICONV "Override: afdko" OFF)
+option(LIBXML2_WITH_LZMA "Override: afdko" OFF)
+option(LIBXML2_WITH_ZLIB "Override: afdko" OFF)
 
-file(MAKE_DIRECTORY ${SOURCE_DIR}/include)
-
-add_library(libxml2 STATIC IMPORTED GLOBAL)
-
-add_dependencies(libxml2 ${LIBXML2_TARGET})
-set_target_properties(libxml2 PROPERTIES
-  IMPORTED_LOCATION ${CMAKE_CURRENT_BINARY_DIR}/${LIBXML2_OUTPUT_DIR}/${LIBXML2_LIBNAME}
-  INTERFACE_INCLUDE_DIRECTORIES ${SOURCE_DIR}/include)
-target_include_directories(libxml2 INTERFACE ${SOURCE_DIR}/include INTERFACE ${BINARY_DIR})
-
-set(LIBXML2_STATIC_INCLUDE_DIR ${SOURCE_DIR}/include ${BINARY_DIR})
-
-set(LIBXML2_WIN_LIBRARIES wsock32 ws2_32 libxml2)
-
-set(LIBXML2_NONWIN_LIBRARIES libxml2)
+if(CMAKE_VERSION VERSION_LESS 3.24 AND DEFINED ENV{FORCE_SYSTEM_LIBXML2})
+  find_package(LibXml2 REQUIRED CONFIG)
+else()
+  if(DEFINED ENV{FORCE_SYSTEM_LIBXML2})
+    set(CMAKE_REQUIRE_FIND_PACKAGE_LibXml2 ON)
+  endif()
+  FetchContent_MakeAvailable(LibXml2)
+endif()
